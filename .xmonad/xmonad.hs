d58ee2a7b9116239584a8a444f923ee51d0c930b
Elliot Wolk <elliot.wolk@gmail.com>
2011-05-22 14:54:42 -0400

    added bindings for setting the scaling governor, win+F1/2/3/4 for cpu0 and win +F5/6/7/8 for cpu1

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -188,6 +188,16 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = fromList $
   , ((alt , xK_Menu  ), spawn "$HOME/bin/nautilusDesktop toggle")
   , ((ctrl, xK_Menu  ), spawn "$HOME/bin/touchClick toggle")
 
+  , ((supr, xK_F1    ), spawn "sudo $HOME/bin/cpu_governor 0 ondemand")
+  , ((supr, xK_F2    ), spawn "sudo $HOME/bin/cpu_governor 0 conservative")
+  , ((supr, xK_F3    ), spawn "sudo $HOME/bin/cpu_governor 0 powersave")
+  , ((supr, xK_F4    ), spawn "sudo $HOME/bin/cpu_governor 0 performance")
+
+  , ((supr, xK_F5    ), spawn "sudo $HOME/bin/cpu_governor 1 ondemand")
+  , ((supr, xK_F6    ), spawn "sudo $HOME/bin/cpu_governor 1 conservative")
+  , ((supr, xK_F7    ), spawn "sudo $HOME/bin/cpu_governor 1 powersave")
+  , ((supr, xK_F8    ), spawn "sudo $HOME/bin/cpu_governor 1 performance")
+
   , ((alct, xK_space ), spawn "gnome-system-monitor")
 
   , ((supr, xK_space ), spawn "$HOME/bin/rc-start --play-pause")
