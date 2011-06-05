fb346c5319d7257b3f94ff36aeaded9d32b54966
Elliot Wolk <elliot.wolk@gmail.com>
2011-06-04 23:29:33 -0400

    removed unused imports

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -1,34 +1,14 @@
 import Bindings
 import Dzen
-import DzenXinerama
 
 import XMonad hiding ( (|||) )
 import XMonad.Layout.LayoutCombinators ( (|||) )
-import XMonad.Layout.IndependentScreens (countScreens)
-import XMonad.Layout.Named
-import XMonad.Hooks.DynamicLog
-import XMonad.Hooks.ManageDocks
-import XMonad.Hooks.UrgencyHook
-import XMonad.Actions.NoBorders (toggleBorder)
-import XMonad.Layout.NoBorders (smartBorders, noBorders)
-import XMonad.Actions.CopyWindow (copyToAll, killAllOtherCopies)
-import XMonad.Actions.DeManage (demanage)
-import XMonad.Util.Font (encodeOutput)
-import XMonad.Util.Loggers
-import XMonad.Util.NamedWindows (getName)
-import XMonad.Util.Run
+
+import XMonad.Hooks.ManageDocks (avoidStruts)
+import XMonad.Layout.Named (named)
+import XMonad.Layout.NoBorders (smartBorders)
 
 import qualified XMonad.StackSet as Stk
-import Data.Monoid
-import Data.Function (on)
-import Data.Ord (comparing)
-import Data.Map (fromList)
-import Data.Maybe (isJust, catMaybes)
-import Data.List (sortBy, intercalate)
-
-import Control.Monad (zipWithM_)
-
-import System.Exit
 
 workspaceNames = ["A", "B", "D", "G", "5", "6", "7", "8", "9"]
 
@@ -50,9 +30,10 @@ main = do
     mouseBindings      = myMouseBindings,
   
     layoutHook         = avoidStruts $ smartBorders $
-                         (named "left" $          Tall 1 (3/100) (55/100)) |||
-                         (named "top"  $ Mirror $ Tall 1 (3/100) (55/100)) |||
-                         (named "full" $          Full),
+                             (named "left" $          Tall 1 (3/100) (55/100))
+                         ||| (named "top"  $ Mirror $ Tall 1 (3/100) (55/100))
+                         ||| (named "full" $          Full)
+                         ,
 
     manageHook         = composeAll
                          [ className =? "Gnome-panel"    --> doIgnore
