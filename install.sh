53e85977de0da7dd10d5c02083c96c85e9c3590d
Elliot Wolk <elliot.wolk@gmail.com>
2011-06-07 15:57:50 -0400

    added kernel-package to free packages installed

--- install.sh
+++ install.sh
@@ -160,7 +160,7 @@ if [ "$REPLY" == "y" ]; then
     xsel flac libsvn-java xtightvncviewer x11vnc gvfs-bin git \
     gnome-common ttf-inconsolata gimp ffmpeg wmctrl xsane php5 \
     libxslt1-dev libgdbm-dev rhythmbox link-monitor-applet gnome-do \
-    librsvg2-bin fbreader xdotool powertop
+    librsvg2-bin fbreader xdotool powertop kernel-package
   sudo apt-get install -y alarm-clock-applet
   sudo apt-get install -y aptitude
 fi
