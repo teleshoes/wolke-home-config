a93b408a10b1cc86d15c2dc3be31bcc7daa7d991
Elliot Wolk <elliot.wolk@gmail.com>
2011-09-29 23:29:58 -0400

    made CommandClick.hs a module

--- .dzen2/printers/CommandClick.hs
+++ .dzen2/printers/CommandClick.hs
@@ -1,3 +1,4 @@
+module CommandClick (commandClick, imgDir) where
 import System.Environment.UTF8 (getArgs)
 import System.Environment (getEnv)
 import ShiftUp (shiftUp)
@@ -8,8 +9,10 @@ main = do
  args <- getArgs
  home <- getEnv "HOME"
  putStr $ commandClick home (args !! 0)
- 
+
+imgDir home = home ++ "/.dzen2/icons/36x36/"
+
 commandClick home exec = clickableImage [exec] img
-  where img = home ++ "/.dzen2/icons/36x36/" ++ lastPathElem exec ++ ".xpm"
+  where img = imgDir home ++ exec ++ ".xpm"
 
 lastPathElem p = reverse $ takeWhile (/='/') (reverse p)
