09f2db06160974580d8889b845e0a5a499d16f83
Elliot Wolk <elliot.wolk@gmail.com>
2011-10-01 02:34:19 -0400

    added make clean

--- utils/thinkpad_acpi/install.sh
+++ utils/thinkpad_acpi/install.sh
@@ -2,8 +2,9 @@
 dir=/lib/modules/`uname -r`/kernel/drivers/platform/x86/
 mod=thinkpad_acpi.ko
 make
-if [ -e thinkpad_acpi.ko]; then
+if [ -e thinkpad_acpi.ko ]; then
   echo coping $mod to $dir, backing up orig as $mod.orig
   sudo mv $dir/$mod $dir/$mod.orig
   sudo cp $mod $dir
 fi
+make clean
