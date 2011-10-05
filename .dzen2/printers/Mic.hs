720ffd50395ad1b86181f93b8de71e25c55ad350
Elliot Wolk <elliot.wolk@gmail.com>
2011-10-05 12:53:09 -0400

    made microphone mute a circle

--- .dzen2/printers/Mic.hs
+++ .dzen2/printers/Mic.hs
@@ -4,18 +4,23 @@ import CommandClick (imgDir)
 import ClickAction (clickAction)
 import ClickableImage (clickableImage)
 
+height = 36
+
 main = do
  home <- getEnv "HOME"
  mic <- readProcess (home ++ "/bin/pulse-mute") ["microphone"] ""
- putStr $ format' home (isMuted $ lines mic !! 0)
+ putStr $ formatCircle home (isMuted $ lines mic !! 0)
 
-format home isM = clickableImage [clickCmd] $ imgDir home ++ img
+formatImg home isM = clickableImage [clickCmd] $ imgDir home ++ img
   where img | isM     = "microphone-muted.xpm"
             | not isM = "microphone-unmuted.xpm"
 
-format' home isM = clickAction "1" clickCmd markup
-  where markup = "^bg(black)^fg(" ++ color ++ ")M^fg()^bg()"
-        color = if isM then "red" else "green"
+formatCircle home isM = clickAction "1" clickCmd markup
+  where markup = color $ "^p(;" ++ (show shift) ++ ")^c(" ++ (show d) ++ ")"
+        d = height `div` 6
+        shift = height `div` 2 - d `div` 2
+        color m = "^fg(" ++ fg ++ ")" ++ m ++ "^fg()"
+        fg = if isM then "red" else "green"
 
 isMuted mic | mic == "source is muted" = True
             | mic == "source is not muted" = False
