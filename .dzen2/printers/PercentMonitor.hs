6ae063f30552fe150c9ea71790c14dae8ba45ec3
Elliot Wolk <elliot.wolk@gmail.com>
2011-10-12 13:36:09 -0400

    replaced sequences with lists coz

--- .dzen2/printers/PercentMonitor.hs
+++ .dzen2/printers/PercentMonitor.hs
@@ -7,17 +7,16 @@ import Control.Monad
 import Control.Applicative((<$>))
 import Control.Exception
 import System.Environment.UTF8 (getArgs)
-import qualified Data.Sequence as S
 
 main = do
   (w,h,colors) <- parseArgs <$> getArgs
   let loop i oldSamples = do
-      samples <- (pushPop oldSamples) . getPercents <$> getLine
+      samples <- (:(pop oldSamples)) . getPercents <$> getLine
       print $ formatSamples h samples
       loop ((i+1) `mod` w) samples
-  loop 0 $ S.replicate w []
+  loop 0 $ replicate w []
 
-pushPop seq a = case S.viewr (a S.<| seq) of newSeq S.:> _ -> newSeq
+pop = reverse.tail.reverse
 
 i = read :: String -> Int
 f = read :: String -> Float
@@ -37,13 +36,13 @@ getPercents line | isPer = map f $ pers
   where pers = words line
         isPer = all isFloat pers && length pers > 0
 
-formatSamples :: Int -> S.Seq [Float] -> String
+formatSamples :: Int -> [[Float]] -> String
 formatSamples height samples = markup --"^ib(1)" ++ markup ++ "^ib(0)^fg()^pa()"
-  where px = fmap (pixels height) (S.filter (not . null) samples)
+  where px = fmap (pixels height) (filter (not . null) samples)
         markup = Foldable.concatMap show px
 
 drawRectStack width heights colors = addResets $ map (rect width) hcos
-  where hcos = S.filter ((>0).first) $ zip3 heights colors (0 S.<| heights)
+  where hcos = filter ((>0).first) $ zip3 heights colors (0:heights)
         addResets = intersperse ("^p(-" ++ (show width) ++ ")")
         first (x,_,_) = x
 
@@ -52,7 +51,7 @@ rect width (height,color,offset) = "^pa(;" ++ offset ++ ")" ++
                                    "^fg(" ++ color ++ ")" ++
                                    "^r(" ++ width ++ "x" ++ height ++ ")"
 
-pixels :: Int -> S.Seq [Float] -> S.
+pixels :: Int -> [[Float]] -> [[Int]]
 pixels totalHeight samplePers = ensureHeight totalHeight $ fmap px samplePers
   where px 0.0 = 0
         px per = if hpx per == 0 then 1 else hpx per
