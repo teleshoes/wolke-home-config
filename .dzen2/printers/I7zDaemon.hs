b1a4eeaf27b530deee8dacf8aef1338a3da41115
Elliot Wolk <elliot.wolk@gmail.com>
2011-10-14 23:07:42 -0400

    first pass of new i7z daemon

---
+++ .dzen2/printers/I7zDaemon.hs
@@ -0,0 +1,64 @@
+import System.Process (system)
+import System.Posix.Process (getProcessID, forkProcess, executeFile)
+import System.Unix.Process (killByCwd)
+import System.Directory (setCurrentDirectory)
+import Data.Time.Clock.POSIX (getPOSIXTime)
+import Data.Maybe (listToMaybe)
+import Control.Applicative ((<$>))
+import Control.Concurrent (forkIO, threadDelay)
+import Control.Monad (forever, when)
+
+
+basedir = "/tmp/i7z"
+pidfile = basedir ++ "/pid"
+logFile subdir = subdir ++ "/cpufreq.txt"
+
+i7zCmdArr dir =
+  [ "gnome-terminal"
+  , "--working-directory", dir
+  , "--title", "xmonad-hidden"
+  , "-e", "sudo i7z -w l > /dev/null"
+  ]
+mkdirExec dir = "mkdir -p " ++ dir ++ " > /dev/null"
+
+forkExec (cmd:args) = forkProcess $ executeFile cmd True args Nothing
+
+toInt = read :: String -> Int
+isInt = maybe False id . fmap (null . snd) . listToMaybe . (reads :: ReadS Int)
+
+sys = (>> return ()) . system
+
+maybeWriteFile :: FilePath -> String -> IO ()
+maybeWriteFile f s = catch (writeFile f s) (\_ -> return ())
+
+maybeReadFile :: FilePath -> IO (Maybe String)
+maybeReadFile f = catch (Just <$> readFile f) (\_ -> return Nothing)
+
+parsePidFile :: Maybe String -> Int
+parsePidFile Nothing = -1
+parsePidFile (Just out) = pid
+  where outLines = filter (not.null) $ lines $ out
+        onlyLine = if length outLines == 1 then head outLines else ""
+        pid = if isInt onlyLine then toInt onlyLine else -1
+
+void :: IO a -> IO ()
+void = (>> return ())
+
+main = do
+  pid <- getProcessID
+  
+  pidfileOut <- maybeReadFile pidfile
+  let oldpid = parsePidFile pidfileOut
+  when (oldpid > 0) $ void (putStrLn $ basedir ++ "/" ++ show oldpid)
+
+  let dir = basedir ++ "/" ++ show pid
+  system $ mkdirExec dir
+  
+  maybeWriteFile pidfile $ show pid
+  
+  i7zPid <- forkExec $ i7zCmdArr dir
+  print pid
+
+
+--  forever $ putStrLn $ i7zExec
+
