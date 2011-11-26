e6ed5fec04730c0abbe7af45b12f21c0df65a903
Elliot Wolk <elliot.wolk@gmail.com>
2011-11-26 18:38:31 -0500

    fixed up code for latest changes to i7z {nogui, logfile}. cleaned up a lot

--- .dzen2/printers/I7zDaemon.hs
+++ .dzen2/printers/I7zDaemon.hs
@@ -1,69 +1,57 @@
-import System.Process (system, readProcessWithExitCode)
-import System.IO (stdin, stdout, stderr)
-import System.Exit(ExitCode(ExitSuccess))
-import System.Posix (sleep)
-import System.Posix.Process (getProcessID, forkProcess, executeFile)
-import System.Unix.Process (killByCwd)
-import System.Directory (setCurrentDirectory)
-import Data.Time.Clock.POSIX (getPOSIXTime)
+module I7zDaemon (main) where
+import Control.Monad (forever)
+import Data.List (intercalate)
+import Data.Maybe (fromMaybe, listToMaybe)
 import Data.Maybe (listToMaybe, fromMaybe)
-import Control.Applicative ((<$>))
-import Control.Concurrent (forkIO, threadDelay)
-import Control.Monad (forever, when, void)
-import GHC.IO.Handle (hClose)
+import System.IO (hGetLine, hClose, stdin, stdout, stderr)
+import System.Process (runInteractiveProcess)
+import System.Posix.Process (forkProcess, executeFile, getProcessID)
+import Text.Regex.PCRE ((=~))
 
-basedir = "/tmp/i7z"
-logFileName = "cpu_freq_log.txt"
+main = do
+  pid <- getProcessID
+  let logfile = "/tmp/i7z_" ++ show pid ++ ".log"
 
-i7zCmdArr dir =
-  [ "sh"
-  , "-c"
-  , "cd " ++ dir ++ "; sudo i7z -w l > /dev/null"
+  forkExec $ i7zCmdArr logfile
+  logFH <- tailFile logfile
+
+  forever $ do
+    line <- hGetLine logFH
+    let freqs = parseI7zLog line
+    putStr $ formatFreqs freqs
+
+
+sh cmd = ["sh", "-c", intercalate " " cmd]
+
+i7zCmdArr log = sh
+  [ "sudo"
+  , "i7z"
+  , "--nogui"
+  , "-w", "a"
+  , "--logfile", log
   ]
-
-mkdirExec dir = "mkdir -p " ++ dir ++ " >/dev/null 2>/dev/null"
 
 forkExec (cmd:args) = forkProcess $ do
   hClose stdin
   hClose stdout
   hClose stderr
-  readProcessWithExitCode cmd args ""
---  executeFile cmd True args Nothing
+  executeFile cmd True args Nothing
+
+tailFile f = do
+  let args = [ "-F", f
+             , "-n", "0"
+             ]
+  (_,fh,_,_) <- runInteractiveProcess "tail" args Nothing Nothing
+  return fh
+
+formatFreqs fs = (intercalate " " (map show fs)) ++ "\n"
+
+numbers s = concat groupSets
+  where groupSets = map tail (s =~ p :: [[String]])
+        p = "(\\d+(?:\\.\\d+)?)"
 
 toDouble = read :: String -> Double
-isDouble = maybe False id . fmap (null . snd) . listToMaybe . (reads :: ReadS Double)
 
-maybeCatFile :: FilePath -> IO (Maybe String)
-maybeCatFile f = do
-  (exit, out, err) <- readProcessWithExitCode "cat" [f] ""
-  return $ if exit == ExitSuccess then Just out else Nothing
-
-main = do
-  pid <- getProcessID
-  let dir = basedir ++ "/" ++ show pid
-  let log = dir ++ "/" ++ logFileName
-
-  system $ mkdirExec dir
-  forkExec $ i7zCmdArr dir
-
-  untilReady log
-
-  system $ "stty sane"
-
-  forever $ do
-    system "sleep 1"
-    freqs <- maybeParseFreqs <$> maybeCatFile log
-    if null freqs then putStrLn "blargh" else putStrLn $ show freqs
-
-
-untilReady log = do
-  sleep 1
-  putStrLn "waiting"
-  mFreqs <- maybeParseFreqs <$> maybeCatFile log
-  if null mFreqs then untilReady log else return ()
-
-maybeParseFreqs Nothing = []
-maybeParseFreqs (Just out) = if not ok then [] else map show freqs
-  where lns = lines out
-        ok = all isDouble lns
-        freqs = filter (<100000) $ map (round.toDouble) lns
+--"bignumber Mhz Mhz ... " -> [Mhz]
+parseI7zLog :: String -> [Integer]
+parseI7zLog line = filter (<10^9) $ map (round.toDouble) $ numbers line
