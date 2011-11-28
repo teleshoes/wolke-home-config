a2bd30b171954668e43fe9ca7c7b2f19d63ea58a
Elliot Wolk <elliot.wolk@gmail.com>
2011-11-28 15:49:14 -0500

    added clickable ssids, but it doesnt work because dzen is shitty

--- .dzen2/printers/WScan.hs
+++ .dzen2/printers/WScan.hs
@@ -1,10 +1,12 @@
 module WScan(main) where
+import Data.Char (isSpace)
 import Data.Maybe (fromMaybe, listToMaybe, catMaybes)
+import Data.List (intercalate)
 import System.Process (readProcess, readProcessWithExitCode)
 import System.Exit (ExitCode (ExitSuccess))
 import System.Environment (getEnv)
 import System.IO (hFlush, stdout)
-import Control.Monad (void)
+import Control.Monad (void, forever)
 import Control.Monad.Loops (whileM, iterateUntil)
 import System.Posix (sleep)
 import Text.Regex.PCRE
@@ -17,23 +19,40 @@ main = do
   putStrLn $ "<last scan below, fetching new now>"
   old <- readProcess "wscan" ["-l"] ""
   putStrLn $ formatWscan old
-  iterateUntil success (do
-    hFlush stdout
-    (exitCode, out, err) <- readProcessWithExitCode "wscan" [] ""
-    if success exitCode
-    then putStrLn $ "^cs()\n" ++ formatWscan out
-    else (do
-      putStrLn $ "err: " ++ errorMsg err
-      void $ sleep 1)
-    return exitCode)
+  forever scan
 
-formatWscan = id
---  title = "Wifi Scan: " ++ concat  (map wrapWscan) . lines
+scan = iterateUntil success $ do
+  hFlush stdout
+  (exitCode, out, err) <- readProcessWithExitCode "wscan" [] ""
+  if success exitCode
+  then do
+    date <- readProcess "date" [] ""
+    putStrLn $ "^cs()"
+    putStrLn $ date
+    putStrLn $ formatWscan out
+  else (do
+    putStrLn $ "err: " ++ errorMsg err
+    void $ sleep 1)
+  return exitCode
 
---getSavedSSIDs = catMaybes . (map isSaved)
+trimR = reverse . (dropWhile isSpace) . reverse
+
+formatWscan s = "^tw()" ++ title ++ "\n" ++ s
+  where savedSSIDs = map trimR $ catMaybes $ map maybeSavedSSID $ lines s
+        title = "Wifi Scan: ( " ++ concatMap clickableSSID savedSSIDs ++ " )"
+
+clickableSSID ssid = ""
+  ++ " "
+  ++ "^ca(1," ++ cmd ++ ")"
+    ++ "^fg(green)"
+    ++ ssid
+    ++ "^fg()"
+  ++ "^ca()"
+  ++ " "
+  where cmd = "sudo wconnect '" ++ ssid ++ "'"
 
 maybeSavedSSID line = if saved then Just (matches !! 0) else Nothing
-  where matches = getMatches line "(.{20}) \\| (.{7}) \\| (.{4}) \\| (.{5})"
+  where matches = getMatches line "(.{20}) \\| (.{4}) \\| (.{4}) \\| (.{5})"
         hasAuto autoStr = autoStr =~ "^\\[" :: Bool
         saved = (length matches == 4) && (hasAuto $ matches !! 3)
 
