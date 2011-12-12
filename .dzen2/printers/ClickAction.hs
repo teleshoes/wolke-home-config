92e545c0d1ead4481a814b83ea2cfc79b33cf7cb
Elliot Wolk <elliot.wolk@gmail.com>
2011-12-11 20:56:53 -0500

    combined the main-ish and modular-ish click-action haskell files and cleaned then up

--- .dzen2/printers/ClickAction.hs
+++ .dzen2/printers/ClickAction.hs
@@ -1,9 +1,17 @@
-module ClickAction (clickAction) where
+module ClickAction (main, clickAction) where
 import System.Environment.UTF8 (getArgs)
 
-clickAction button cmd m = ""
+main = do
+  args <- getArgs
+  let [btn,cmd,mrk] = case length args of
+                      2 -> "1":args
+                      3 -> args
+                      _ -> error "Usage: <optional button> command markup"
+  putStr $ clickAction btn cmd mrk
+
+clickAction btn cmd mrk = ""
   ++ "^pa(;0)"
-  ++ "^ca(" ++ button ++ ", " ++ cmd ++ ")"
-  ++ m
+  ++ "^ca(" ++ btn ++ ", " ++ cmd ++ ")"
+  ++ mrk
   ++ "^ca()"
   ++ "^pa()"
