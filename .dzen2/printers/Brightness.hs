f2a8128fa8d0c44206e6d75ea8baab65b0fde32e
Elliot Wolk <elliot.wolk@gmail.com>
2012-01-15 22:29:20 -0500

    call brightness last to reset brightness

--- .dzen2/printers/Brightness.hs
+++ .dzen2/printers/Brightness.hs
@@ -1,9 +1,10 @@
 module Brightness (main) where
 import System.Environment (getEnv)
-import System.Process(readProcess)
+import System.Process(readProcess, system)
 import PercentBar (percentBar)
 
 main = do
+  system "$HOME/bin/brightness last > /dev/null"
   brightness <- fmap (round.d) $ readProcess "xbacklight" ["-get"] ""
   let colors = ["black", "darkgray"] ++ repeat "orange"
   putStr $ percentBar brightness colors 5 3
