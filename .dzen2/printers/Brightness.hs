8431fc0997e089dc6568e15a07f0fce2e14c7032
Elliot Wolk <elliot.wolk@gmail.com>
2012-02-08 21:47:18 -0500

    maybe i shouldnt run this on a loop forever without stopping for breath all in one sentence

--- .dzen2/printers/Brightness.hs
+++ .dzen2/printers/Brightness.hs
@@ -2,8 +2,10 @@ module Brightness (main) where
 import System.Environment (getEnv)
 import System.Process(readProcess, system)
 import PercentBar (percentBar)
+import Control.Concurrent (threadDelay)
 
 main = do
+  threadDelay $ 1*10^6
   system "$HOME/bin/brightness last > /dev/null"
   brightness <- fmap (round.d) $ readProcess "xbacklight" ["-get"] ""
   let colors = ["black", "darkgray"] ++ repeat "orange"
