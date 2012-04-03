7fe2b961fc205f73da7e20bd44e3a44f174ec8c4
Elliot Wolk <elliot.wolk@gmail.com>
2012-04-03 12:07:25 -0400

    replaced wconnect `wguessid` with wauto

--- .xmonad/lib/Bindings.hs
+++ .xmonad/lib/Bindings.hs
@@ -118,7 +118,7 @@ myKeyBindings conf@(XConfig {XMonad.modMask = modm}) = fromList $
   , ((supr, xK_F3    ), spawn "sudo cpu-set powersave")
   , ((supr, xK_F4    ), spawn "sudo cpu-set performance")
 
-  , ((supr, xK_1     ), spawn "sudo wconnect `wguessid`")
+  , ((supr, xK_1     ), spawn "sudo wauto")
   , ((supr, xK_2     ), spawn "sudo wconnect -d; sudo tether off; sudo wired off")
   , ((supr, xK_3     ), spawn "sudo tether on")
   , ((supr, xK_4     ), spawn "sudo wired on")
