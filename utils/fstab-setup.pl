a3323d26741366f98335a5a71164c254982e8620
Elliot Wolk <elliot.wolk@gmail.com>
2012-04-30 20:54:58 -0400

    made a wrapper to write my fstab, safe now that im using labels and not uuids

--- utils/fstab-setup.pl
+++ utils/fstab-setup.pl
@@ -1,7 +1,24 @@
-# /etc/fstab: static file system information.
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+my $fstab = '# /etc/fstab: static file system information.
 # <file system> <mount point>   <type>  <options>       <dump>  <pass>
 proc        /proc           proc    nodev,noexec,nosuid 0 0
 tmpfs       /tmp            tmpfs   defaults,noatime,nodev,nosuid,mode=1777 0 0
 LABEL=root  /               ext4    errors=remount-ro 0 1
 LABEL=home  /home           ext4    defaults 0 2
 LABEL=stuff /media/stuff    ext4    defaults 0 2
+';
+
+my $fstabFile = '/etc/fstab';
+
+print "OLD:\n";
+system "cat $fstabFile";
+print "\n---\n";
+print "NEW:\n";
+print $fstab;
+
+open FH, "| sudo tee $fstabFile > /dev/null";
+print FH $fstab;
+close FH;
