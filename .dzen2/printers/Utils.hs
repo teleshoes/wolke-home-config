b53d4a473f2542c477bf6370a7c0285cf15fa342
Elliot Wolk <elliot.wolk@gmail.com>
2012-05-30 19:02:20 -0400

    added absolute position dzen printer utils

--- .dzen2/printers/Utils.hs
+++ .dzen2/printers/Utils.hs
@@ -1,19 +1,25 @@
-module Utils(height, fg, bg, isRunning) where
+module Utils(height, fg, bg, posAbs, posAbsX, posAbsY, isRunning) where
 import System.Process(runCommand, system)
 import System.Exit(ExitCode(ExitFailure))
 
+-- CONSTANTS
 height = 36
 
+-- PRINTERS
 fg :: String -> String -> String
 fg color markup = "^fg(" ++ color ++ ")" ++ markup ++ "^fg()"
 
 bg :: String -> String -> String
 bg color markup = "^bg(" ++ color ++ ")" ++ markup ++ "^bg()"
 
+posAbs x y m = "^pa(" ++ show x ++ ";" ++ show y ++ ")" ++ m
+posAbsX x m = "^pa(" ++ show x ++ ")" ++ m
+posAbsY y m = "^pa(;" ++ show y ++ ")" ++ m
+
+-- IO
 isRunning :: String -> IO Bool
 isRunning p = do
   running <- system $ "pgrep " ++ p ++ " > /dev/null 2>/dev/null"
   return $ case running of
     ExitFailure _ -> False
     otherwise -> True
-
