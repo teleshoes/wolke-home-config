e2b1cca6764792f449ec41d3d9a031999ed89627
Elliot Wolk <elliot.wolk@gmail.com>
2012-05-30 18:32:25 -0400

    added fg and bg dzen markup utils

--- .dzen2/printers/Utils.hs
+++ .dzen2/printers/Utils.hs
@@ -1,6 +1,12 @@
-module Utils(isRunning) where
+module Utils(fg, bg, isRunning) where
 import System.Process(runCommand, system)
 import System.Exit(ExitCode(ExitFailure))
+
+fg :: String -> String -> String
+fg color markup = "^fg(" ++ color ++ ")" ++ markup ++ "^fg()"
+
+bg :: String -> String -> String
+bg color markup = "^bg(" ++ color ++ ")" ++ markup ++ "^bg()"
 
 isRunning :: String -> IO Bool
 isRunning p = do
