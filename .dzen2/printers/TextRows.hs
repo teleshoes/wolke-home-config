2ff4334e26ab12046bab0962b6bdad8b79acd5d3
Elliot Wolk <elliot.wolk@gmail.com>
2012-05-31 18:56:03 -0400

    replaced markup with dzen utils, fixed pos util calls

--- .dzen2/printers/TextRows.hs
+++ .dzen2/printers/TextRows.hs
@@ -1,6 +1,6 @@
 module TextRows(textRows, main) where
 import System.Environment.UTF8 (getArgs)
-import Utils (height, posAbsY, estimateLength)
+import Utils (height, lockX, posAbsY, shiftMiddle, ignoreBG, estimateLength)
 
 topPx = 0 - (height `div` 12)
 botPx = topPx + (height `div` 2)
@@ -10,15 +10,8 @@ main = do
  putStr $ textRows (args !! 0) (args !! 1)
 
 textRows topText botText = overlapMarkup shorter longer
-  where (top, bot) = (posAbsY topPx topText, posAbsY botPx botText)
+  where (top, bot) = (posAbsY topPx ++ topText, posAbsY botPx ++ botText)
         (topLen, botLen) = (estimateLength top, estimateLength bot)
         (shorter, longer) = if topLen <= botLen then (top, bot) else (bot, top)
 
-overlapMarkup bg fg = ""
-  ++ "^p(_LOCK_X)"
-  ++ bg
-  ++ "^p(_UNLOCK_X)"
-  ++ "^ib(1)"
-  ++ fg
-  ++ "^ib(0)"
-  ++ "^pa()"
+overlapMarkup bgM fgM = lockX bgM ++ ignoreBG fgM ++ shiftMiddle
