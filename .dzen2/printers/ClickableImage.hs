2714d7d5f06ae9ad7157da5fcfb2353bad4689a4
Elliot Wolk <elliot.wolk@gmail.com>
2012-05-31 18:53:35 -0400

    removed redundant abs pos, used dzen utils

--- .dzen2/printers/ClickableImage.hs
+++ .dzen2/printers/ClickableImage.hs
@@ -1,19 +1,17 @@
 module ClickableImage (main, clickableImage) where
 import System.Environment.UTF8 (getArgs)
 import System.Process(readProcess)
-import ClickAction (clickAction)
-import Utils (shiftUp)
+import ClickAction (clickActions)
+import Utils (img)
 
 main = do
  args <- getArgs
- putStr $ shiftUp $ uncurry clickableImage $ parseArgs args
+ putStr $ uncurry clickableImage $ parseArgs args
+
+clickableImage cmds imgPath = clickActions cmds $ img imgPath
 
 parseArgs args | length args == 4 = (take 3 args, args !! 3)
                | length args == 3 = (take 2 args, args !! 2)
                | length args == 2 = (take 1 args, args !! 1)
                | otherwise = error "Usage: btn1Cmd [btn2Cmd] [btn3Cmd] imgpath"
-
-clickableImage cmds image = wrapCmds 1 cmds $ "^i(" ++ image ++ ")"
-wrapCmds cnt (c:cs) m = wrapCmds (cnt+1) cs (clickAction (show cnt) c m)
-wrapCmds _ [] m = m
 
