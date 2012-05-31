ddd651e81c31b305b1be466ffd17b4132935a305
Elliot Wolk <elliot.wolk@gmail.com>
2012-05-31 16:43:50 -0400

    made mike-ish improvements

--- .dzen2/printers/Calendar.hs
+++ .dzen2/printers/Calendar.hs
@@ -3,13 +3,14 @@ import System.Process(readProcess)
 import Data.List (intercalate, intersperse, transpose)
 import Data.List.Split (splitEvery, splitOn)
 import Utils (fg, bg, padL, padR, estimateLength)
+import Control.Arrow (first, second)
 
 main = do
-  [y,m,d] <- sequence $ map runDate ["Y", "m", "d"]
-  cals <- sequence $ map (getCal (y,m,d)) [-23..24]
+  [y,m,d] <- mapM runDate ["Y", "m", "d"]
+  cals <- mapM (getCal (y,m,d)) [-23..24]
   putStr $ formatColumns cals 6
 
-runCal (y,m) = readProcess "cal" ["-h", show m, show y] "" :: IO String
+runCal (y,m) = readProcess "cal" ["-h", show m, show y] ""
 runDate f = fmap read $ readProcess "date" ["+%" ++ f] "" :: IO Int
 
 replaceAll old new list = intercalate new $ splitOn old list
@@ -17,10 +18,10 @@ replaceAll old new list = intercalate new $ splitOn old list
 getCal (y,m,d) monthOffset = fmap style $ runCal $ relMonth monthOffset (y,m)
   where style = if monthOffset == 0 then styleThisMonth d else id
 
-relMonth :: Int -> (Int, Int) -> (Int, Int)
-relMonth n = (!! (abs n)) . iterate (if n >= 0 then nextMonth else prevMonth)
-  where prevMonth (y,m) = if m == 1 then (y-1, 12) else (y, m-1)
-        nextMonth (y,m) = if m == 12 then (y+1, 1) else (y, m+1)
+
+relMonth n = fromAbsMonth . (+n) . toAbsMonth
+  where toAbsMonth   = uncurry (+) . first (*12) . second (subtract 1)
+        fromAbsMonth = second (+1) . (`quotRem` 12)
 
 styleThisMonth d c = unlines $ (bg "blue" header):(map styleDate dateLines)
   where (header:dateLines) = lines c
