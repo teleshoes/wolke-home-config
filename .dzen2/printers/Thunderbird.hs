0a53fe521f0a87e6b3e8715837a2b5a452a900f0
Elliot Wolk <elliot.wolk@gmail.com>
2012-06-01 15:30:49 -0400

    rewrote thunderbird dzen printer in haskell

--- .dzen2/printers/Thunderbird.hs
+++ .dzen2/printers/Thunderbird.hs
@@ -1,134 +1,83 @@
-#!/usr/bin/perl
-use strict;
-use warnings;
+module Thunderbird(main) where
+import Utils (
+  fg, bg,
+  shiftMid,
+  chompAll, padL, isRunning)
+import TextRows (textRows)
+import ClickAction (clickActions)
 
-my $PRINTERS = `echo -n \$HOME/.dzen2/printers`;
-sub ghcprinter(@){
-  my @args = @_;
-  for my $arg(@args){
-    $arg =~ s/\'/'\\''/g;
-    $arg = "'$arg'";
-  }
-  my $out = `$PRINTERS/ghcprinter @args`;
-  chomp $out;
-  return $out;
-}
+import qualified Data.Map as M (fromList, lookup, member)
 
+import Data.Maybe (catMaybes, fromMaybe)
+import System.Environment (getEnv)
+import System.Directory (doesFileExist)
+import System.Process (readProcessWithExitCode)
+import Text.Regex.PCRE ((=~))
 
-my $profileDir = '~/.thunderbird/*.default';
-my $exec = 'thunderbird';
-my $processName = 'thunderbird-bin';
-my $focusWinCmd = 'xdotool key --clearmodifiers alt+8';
+clickCommands = [ ""
+                  ++ "pidof thunderbird-bin; "
+                  ++ "if [ $? == 0 ]; then "
+                    ++ "xdotool key --clearmodifiers alt+8; "
+                  ++ "else "
+                    ++ "thunderbird; "
+                  ++ "fi"
+                , "thunderbird --compose"
+                , "killall thunderbird-bin"
+                ]
 
-my $runningLeftCmd = "xdotool key --clearmodifiers alt+8";
-my $notRunningLeftCmd = "$exec";
-my $leftCmd =
-  "pidof $processName; " .
-  "if [ \$? == 0 ]; " .
-  "then $runningLeftCmd; " .
-  "else $notRunningLeftCmd; " .
-  "fi";
+accounts = M.fromList [ ("Gmail", "G")
+                      , ("LilleGroup", "L")
+                      , ("AOL - LiberiFataliVIII", "A")
+                      ]
 
+getProfileDir = do
+  home <- getEnv "HOME"
+  let cmd = ["find", home ++ "/.thunderbird/", "-iname", "*.default"]
+  (_, out, _) <- readProcessWithExitCode (head cmd) (tail cmd) ""
+  return out
 
-my $lClickCmd = "$leftCmd";
-my $mClickCmd = "$exec -compose";
-my $rClickCmd = "killall $processName";
+main = do
+  home <- getEnv "HOME"
+  let imgSize = 16
+  let imgSubDir = show imgSize ++ "x" ++ show imgSize
+  let imgPath = home ++ "/.dzen2/icons/" ++ imgSubDir ++ "/thunderbird.xpm"
 
-my %ACCOUNTS = (
-  'Gmail' => 'G',
-  'LilleGroup' => 'L',
-  'AOL - LiberiFataliVIII' => 'A');
+  profileDir <- fmap chompAll getProfileDir
+  let ucFile = profileDir ++ "/unread-counts"
 
-sub isThunderbirdRunning();
-sub background($);
-sub green($);
-sub red($);
-sub getCounts();
-sub pad2($);
+  tbRunning <- isRunning "thunderbird-bin"
+  let runningMarkup = if tbRunning then " " else fg "red" "x"
 
-sub main(){
-  my $runningSymbol = '';
+  unreadCountsExists <- doesFileExist ucFile
+  unreadCounts <- if unreadCountsExists then readFile ucFile else return ""
+  let unreadMarkup = formatUnreadCounts $ parseUnreadCounts unreadCounts
 
-  if(isThunderbirdRunning()){
-    $runningSymbol .= green ' ';
-  }else{
-    $runningSymbol .= red 'x';
-  }
+  putStrLn $
+    clickActions clickCommands $
+    bg "black" $ shiftMid ++ runningMarkup ++ fg "green" unreadMarkup
 
-  my %counts = getCounts();
-  my $len = 0;
-  my $max = 0;
-  my $total = 0;
-  for my $count(values %counts){
-    $len++;
-    $max = $count if $count > $max;
-    $total += $count;
-  }
+parseUnreadCounts :: String -> [(String, Integer)]
+parseUnreadCounts uc = catMaybes $ map ucMatch $ lines uc
 
-  my $top;
-  my $bot;
-  if($max > 99 or ($len > 2 && $total > 99)){
-    $top = '99+';
-    $bot = 'all';
-  }elsif($len == 0){
-    $top = '   ';
-    $bot = '   ';
-  }elsif($len == 1){
-    my ($name1) = keys %counts;
-    $top = (pad2 $counts{$name1}) . $name1;
-    $bot = '   ';
-  }elsif($len == 2){
-    my ($name1, $name2) = sort keys %counts;
-    $top = (pad2 $counts{$name1}) . $name1;
-    $bot = (pad2 $counts{$name2}) . $name2;
-  }else{
-    $top = green ' ' . pad2 $total;
-    $bot = 'all';
-  }
+ucMatch :: String -> Maybe (String, Integer)
+ucMatch s = if isMatch && okAccount then Just (display, count) else Nothing
+  where regex = "^(\\d+):(.*)"
+        match = s =~ regex :: [[String]]
+        isMatch = length match == 1
+        count = read $ head match !! 1
+        name = head match !! 2
+        okAccount = M.member name accounts && count > 0
+        display = fromMaybe "" $ M.lookup name accounts
 
-  my $markup = ghcprinter 'TextRows', $top, $bot;
-  $markup = ghcprinter 'ClickAction', 1, $lClickCmd, $markup if $lClickCmd;
-  $markup = ghcprinter 'ClickAction', 2, $mClickCmd, $markup if $mClickCmd;
-  $markup = ghcprinter 'ClickAction', 3, $rClickCmd, $markup if $rClickCmd;
-  $markup = green $markup;
-  $markup = $runningSymbol . $markup;
-  $markup = background $markup;
-  print "$markup\n";
-}
-
-sub background($){
-  return "^bg(black)$_[0]^bg()";
-}
-sub green($){
-  return "^fg(green)$_[0]^fg()";
-}
-sub red($){
-  return "^fg(red)$_[0]^fg()";
-}
-
-sub isThunderbirdRunning(){
-  system "pidof $processName > /dev/null";
-  return $? == 0;
-}
-
-sub getCounts(){
-  my @accounts = `cat $profileDir/unread-counts 2>/dev/null`;
-  my %counts;
-  for my $acc(@accounts){
-    if($acc =~ /^([0-9]+):(.*)\n?$/){
-      my $count = $1;
-      my $name = $2;
-      my $display = $ACCOUNTS{$name};
-      if($count > 0 && defined $display){
-        $counts{$display} = $count;
-      }
-    }
-  }
-  return %counts;
-}
-
-sub pad2($){
-  return ' ' x (2 - (length $_[0])) . $_[0];
-}
-
-main;
+formatUnreadCounts :: [(String, Integer)] -> String
+formatUnreadCounts uc = textRows (padL ' ' 3 top) (padL ' ' 3 bot)
+  where len = length uc
+        counts = map snd uc
+        max = maximum (0:counts)
+        total = sum counts
+        fmtIndex i = if i >= len then "   " else fmt $ uc !! i
+        fmt (disp, cnt) = show cnt ++ disp
+        (top, bot) = if max > 99 || len > 2 then
+                       (if total > 99 then "99+" else show total, "???")
+                     else
+                       (fmtIndex 0, fmtIndex 1)
