b7524c08dc8e8f6de52cdb0efaee20a30358f847
Elliot Wolk <elliot.wolk@gmail.com>
2012-06-04 03:53:27 -0400

    cleaned up memmonitor, used delayedChanReader util

--- .dzen2/printers/MemMonitor.hs
+++ .dzen2/printers/MemMonitor.hs
@@ -1,12 +1,6 @@
 module MemMonitor(main) where
-import Control.Monad (forever)
-import Control.Concurrent (Chan, forkIO, readChan, writeChan, newChan, threadDelay)
-import Data.List (intercalate)
-import Data.Maybe (isJust, fromMaybe, listToMaybe)
-import Text.Regex.PCRE ((=~))
-import Utils (height, lineBuffering, readProc, regexGroups)
+import Utils (height, lineBuffering, readProc, regexGroups, delayedChanReader)
 import PercentMonitor (percentMonitor)
-import System.IO (Handle, hPutStrLn, stdout)
 
 colors = ["#00b25b", "00e575", "#00fe81", "#a9f4c4", "#000000"]
 
@@ -15,15 +9,6 @@ main = do
   let (w, h) = (fromIntegral height, fromIntegral height)
   perChan <- delayedChanReader (fmap freeToPercents $ readProc ["free"]) 1
   percentMonitor w h colors perChan
-
-delayedChanReader :: IO a -> Float -> IO (Chan a)
-delayedChanReader ioA delay = do
-  chan <- newChan
-  forkIO $ forever $ do
-    a <- ioA
-    writeChan chan a
-    threadDelay $ round (delay * 10^6)
-  return chan
 
 freeToPercents :: String -> [Float]
 freeToPercents line = maybe (take 5 $ 100.0:repeat 0) parseFree groups
@@ -34,4 +19,3 @@ parseFree [total, used, free, shared, buffers, cached] = pers
   where percent n = 100.0 * fromIntegral n / fromIntegral total
         pers = map percent [usedReal, shared, buffers, cached, free]
         usedReal = used - shared - buffers - cached
-
