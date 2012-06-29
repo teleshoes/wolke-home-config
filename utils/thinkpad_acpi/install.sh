ee7d053399d74fb62cda6806896a1785f507b2a8
Elliot Wolk <elliot.wolk@gmail.com>
2012-06-29 02:31:32 -0400

    removed a missing symlink

--- utils/thinkpad_acpi/install.sh
+++ utils/thinkpad_acpi/install.sh
@@ -9,16 +9,8 @@ make
 patch -R thinkpad_acpi.c led.patch
 
 if [ -e thinkpad_acpi.ko ]; then
-  echo; echo; echo success!
-  bak="$mod.orig.`date +%s`"
-  echo copying $mod to $dir
-  echo backup in $bak
-  sudo mv $dir/$mod $dir/$bak
+  echo coping $mod to $dir, backing up orig as $mod.orig
+  sudo mv $dir/$mod $dir/$mod.orig.`date +%s`
   sudo cp $mod $dir
-  echo modprobe -r thinkpad_acpi, then modprobe thinkpad_acpi
-  sudo modprobe -r thinkpad_acpi
-  sudo modprobe thinkpad_acpi
 fi
-echo
-echo Cleaning..
 make clean
