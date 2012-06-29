e9bd3cf711cf4b0eb4b56fd7ab5bd1cf2b726ed1
Elliot Wolk <elliot.wolk@gmail.com>
2012-06-29 02:37:17 -0400

    reverted accidental revert; this is kernel 3.3 compatible

--- utils/thinkpad_acpi/install.sh
+++ utils/thinkpad_acpi/install.sh
@@ -9,8 +9,16 @@ make
 patch -R thinkpad_acpi.c led.patch
 
 if [ -e thinkpad_acpi.ko ]; then
-  echo coping $mod to $dir, backing up orig as $mod.orig
-  sudo mv $dir/$mod $dir/$mod.orig.`date +%s`
+  echo; echo; echo success!
+  bak="$mod.orig.`date +%s`"
+  echo copying $mod to $dir
+  echo backup in $bak
+  sudo mv $dir/$mod $dir/$bak
   sudo cp $mod $dir
+  echo modprobe -r thinkpad_acpi, then modprobe thinkpad_acpi
+  sudo modprobe -r thinkpad_acpi
+  sudo modprobe thinkpad_acpi
 fi
+echo
+echo Cleaning..
 make clean
