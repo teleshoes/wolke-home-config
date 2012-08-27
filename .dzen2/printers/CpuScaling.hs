99d2872f814631cf8c96e4c9a52002d02020f9d9
Elliot Wolk <elliot.wolk@gmail.com>
2012-08-26 20:45:30 -0400

    instead of looking at governors, use the scaling_min/max_frequence fields and ignore tmp file; dont ever run cpu-set

--- .dzen2/printers/CpuScaling.hs
+++ .dzen2/printers/CpuScaling.hs
@@ -1,56 +1,59 @@
 module CpuScalingGov(main) where
-import Utils (fg, bg, padL, readInt, chompFile, readProc)
+import Utils (fg, bg, padL, readInt, collectInts, chompFile, runProc, readProc)
 import TextRows (textRows)
 
 import Control.Monad (void)
 import System.Process (system)
-import System.Environment (getEnv)
+import Data.Functor ((<$>))
+import Data.List (sort)
 import Data.Maybe (fromMaybe, isJust, listToMaybe)
 
 width = 2
 
+tmpFile = "/tmp/cpufreq"
 cpuDir = "/sys/devices/system/cpu"
-okGovs = ["powersave", "performance", "ondemand", "conservative", "userspace"]
 
-setGov g = system $ "sudo cpu-set " ++ g
+allSame [] = False
+allSame [_] = True
+allSame (x:y:xs) = x == y && allSame (y:xs)
 
-curGov = do
-  let regex = cpuDir ++ "/cpu[0-9]+/cpufreq/scaling_governor"
-  cpuGovDevs <- fmap lines $ readProc ["find", cpuDir, "-regex", regex]
-  cpuGovs <- mapM chompFile cpuGovDevs
-  let cpuGov = fromMaybe "?" $ listToMaybe cpuGovs
-  return $ if any ((/=) cpuGov) cpuGovs then "?" else cpuGov
+getCpuField f = do
+  let regex = cpuDir ++ "/cpu[0-9]+/cpufreq/scaling_" ++ f
+  devices <- fmap lines $ readProc ["find", cpuDir, "-regex", regex]
+  vals <- mapM chompFile devices
+  return $ if allSame vals then listToMaybe vals else Nothing
+
+getCpuFieldInt f = readInt <$> fromMaybe "" <$> getCpuField f
+getCpuFieldInts f = collectInts <$> fromMaybe "" <$> getCpuField f
+
+readTmp = collectInts <$> chompFile tmpFile
+
+die s = do print s; error s
+maybeDie s x = if not $ isJust x then die s else return $ fromMaybe (error s) x
+check gov minKHz maxKHz avail = do
+  maybeDie "No governor!" gov
+  maybeDie "No min freq!" minKHz
+  maybeDie "No max freq!" maxKHz
+  if null avail then die "No available frequences!" else return ()
 
 main = do
-  home <- getEnv "HOME"
-  cur <- curGov
-  last <- chompFile $ home ++ "/.cpu_governor"
-  let (curOk, lastOk) = (cur `elem` okGovs, last `elem` okGovs)
+  gov <- getCpuField "governor"
+  minKHz <- getCpuFieldInt "min_freq"
+  maxKHz <- getCpuFieldInt "max_freq"
+  avail <- sort <$> getCpuFieldInts "available_frequencies"
+  check gov minKHz maxKHz avail
+  putStrLn $ formatScaling (fm "" gov) (fm 0 minKHz) (fm 0 maxKHz) avail
+  where fm = fromMaybe
 
-  let govDisp = if curOk && lastOk && cur == last then cur else "!"
+formatScaling gov minKHz maxKHz avail = col $ textRows (pad top) (pad bot)
+  where col = color minKHz maxKHz avail
+        pad = padL '0' width . take width
+        (top, bot) = (show $ minKHz `div` 10^5, show $ maxKHz `div` 10^5)
 
-  if lastOk && cur /= last then void $ setGov last else return ()
-  if not lastOk && curOk then void $ setGov cur else return ()
-
-  let spdFile = cpuDir ++ "/cpu0/cpufreq/scaling_setspeed"
-  spd <- if cur == "userspace" then chompFile spdFile else return ""
-  putStrLn $ formatGovernor govDisp (readInt spd)
-
-formatGovernor g spd = fg "black" $ bg color $ textRows (pad top) (pad bot)
-  where spdKHhz = (fromMaybe 0 spd) `div` 10000
-        isTurboBoost = ((fromMaybe 0 spd) `mod` 10000) > 0
-        turboDisp = if isTurboBoost then "turbo" else ""
-        color = govColor g
-        pad = padL ' ' width
-        (top, bot) = if g == "userspace" then
-                       (take width $ show spdKHhz, take width turboDisp)
-                     else
-                       (take width g, take width $ drop width g)
-
-govColor gov = case gov of
-  "powersave"    -> "red"
-  "performance"  -> "green"
-  "ondemand"     -> "blue"
-  "conservative" -> "yellow"
-  "userspace"    -> "purple"
-  _              -> "white"
+color min max avail
+  | min == low && max == high = bg "blue"
+  | min == low && max == low = bg "red" . fg "black"
+  | min == high && max == high = bg "green". fg "black"
+  | otherwise = bg "orange" . fg "black"
+  where low = if null avail then 0 else head avail
+        high = if null avail then 0 else last avail
