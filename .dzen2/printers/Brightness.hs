fe3a1da852541c095dedee595ec36b410a355064
Elliot Wolk <elliot.wolk@gmail.com>
2012-09-03 05:34:37 -0400

    dzen brightness bar: dont fail if xbacklight -get fails, and default to brightness = 300% to show that its weird when theres an error {results in an orange bar}

--- .dzen2/printers/Brightness.hs
+++ .dzen2/printers/Brightness.hs
@@ -3,13 +3,16 @@ import System.Environment (getEnv)
 import System.Process(readProcess, system)
 import PercentBar (percentBar)
 import Control.Concurrent (threadDelay)
+import Data.Maybe (fromMaybe)
+import Data.Functor ((<$>))
+import Utils (readDouble, readProc)
 
 main = do
   threadDelay $ 1*10^6
   system "$HOME/bin/brightness last > /dev/null"
-  brightness <- fmap (round.d) $ readProcess "xbacklight" ["-get"] ""
+  brightness <- parse <$> readProc ["xbacklight", "-get"]
   let colors = ["black", "darkgray"] ++ repeat "orange"
   putStr $ percentBar brightness colors 5
 
-d = read :: String -> Double
+parse = round . fromMaybe 300.0 . readDouble
 
