9983604c54323b26e7fdba2b1af6853cab879f2c
Elliot Wolk <elliot.wolk@gmail.com>
2013-04-04 23:28:49 -0400

    pingMonitor dzen=>taffybar

--- .config/taffybar/lib/PingMonitor.hs
+++ .config/taffybar/lib/PingMonitor.hs
@@ -1,24 +1,21 @@
-module PingMonitor (main) where
-import Utils (fg, lineBuffering, procSuccess)
+module PingMonitor (pingMonitorLabel) where
+import Label(lbl)
+import Utils (fg, procSuccess)
 import System.Environment (getEnv)
-import Control.Concurrent (threadDelay)
+import Control.Concurrent (forkIO, threadDelay, readChan, writeChan, newChan)
 import System.Environment.UTF8 (getArgs)
 
-ping url timeout = procSuccess ["ping", url, "-c", "1", "-w", timeout]
+ping url timeout = procSuccess ["ping", url, "-c", "1", "-w", show timeout]
 
-main = do
-  lineBuffering
-  (url, display, timeout) <- fmap parseArgs getArgs
-  putStrLn $ "?" ++ display
-  pingMonitorLoop url display timeout [fg "purple" "/", fg "green" "|"]
+pingMonitorLabel url display timeout = do
+  chan <- newChan
+  writeChan chan $ "?" ++ display
+  let prefixes = cycle [fg "purple" "/", fg "green" "|"]
+  forkIO $ mapM_ (pingMonitor chan url display timeout) prefixes
+  lbl timeout $ readChan chan
 
-parseArgs (url:display:timeout:[]) = (url,display,timeout)
-parseArgs _ = error "Usage: url display timeout"
-
-pingMonitorLoop u d t = mapM_ (pingMonitor u d t) . cycle
-
-pingMonitor url display timeout prefix = do
+pingMonitor chan url display timeout prefix = do
   isUp <- ping url timeout
   let (color, wait) = if isUp then ("yellow", 3) else ("red", 1)
-  putStrLn $ prefix ++ fg color display
+  writeChan chan $ prefix ++ fg color display
   threadDelay $ wait * 10^6
