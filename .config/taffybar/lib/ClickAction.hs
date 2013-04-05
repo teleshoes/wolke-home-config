1d47faac560416f577033d7c250cc54a303a5033
Elliot Wolk <elliot.wolk@gmail.com>
2013-04-04 23:27:05 -0400

    made dummy clickAction and textRows

--- .config/taffybar/lib/ClickAction.hs
+++ .config/taffybar/lib/ClickAction.hs
@@ -1,16 +1,12 @@
-module ClickAction (main, clickAction, clickActions) where
+module ClickAction (clickAction, clickActions) where
 import System.Environment.UTF8 (getArgs)
 import Utils (clickArea, shiftTop, shiftMid)
 
-main = do
-  args <- getArgs
-  let [btn,cmd,mrk] = case length args of
-                      2 -> "1":args
-                      3 -> args
-                      _ -> error "Usage: <optional button> command markup"
-  putStr $ clickAction (read btn :: Int) cmd mrk
+clickActions cmds m = m
+clickAction btn cmd m = m
 
-clickActions cmds m = shiftTop ++ markup ++ shiftMid
+
+clickActions' cmds m = shiftTop ++ markup ++ shiftMid
   where markup = foldr ($) m (zipWith clickArea [1..] cmds)
 
-clickAction btn cmd m = shiftTop ++ clickArea btn cmd m ++ shiftMid
+clickAction' btn cmd m = shiftTop ++ clickArea btn cmd m ++ shiftMid
--- .config/taffybar/lib/TextRows.hs
+++ .config/taffybar/lib/TextRows.hs
@@ -1,17 +1,5 @@
-module TextRows(textRows, main) where
+module TextRows(textRows) where
 import System.Environment.UTF8 (getArgs)
 import Utils (height, lockX, posAbsY, shiftMid, ignoreBG, estimateLength)
 
-topPx = 0 - (height `div` 12)
-botPx = topPx + (height `div` 2)
-
-main = do
- args <- getArgs
- putStr $ textRows (args !! 0) (args !! 1)
-
-textRows topText botText = overlapMarkup shorter longer
-  where (top, bot) = (posAbsY topPx ++ topText, posAbsY botPx ++ botText)
-        (topLen, botLen) = (estimateLength top, estimateLength bot)
-        (shorter, longer) = if topLen <= botLen then (top, bot) else (bot, top)
-
-overlapMarkup bgM fgM = lockX bgM ++ ignoreBG fgM ++ shiftMid
+textRows topText botText = topText ++ "\n" ++ botText
