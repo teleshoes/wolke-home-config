09b9ffc29ad6f22444c247812797a0b721a2dae8
Elliot Wolk <elliot.wolk@gmail.com>
2013-04-09 03:32:40 -0400

    use tpbattstat json printer and jsonwidget

--- .config/taffybar/lib/TPBattStat.hs
+++ .config/taffybar/lib/TPBattStat.hs
@@ -1,3 +1,16 @@
-module TPBattStat (main) where
-import System.Process (system)
-main = system "/usr/lib/tpbattstat-applet/tpbattstat.py --dzen 2500"
+module TPBattStat (tpBattStatW) where
+import Widgets (label)
+import JsonWidget (jsonWidgetNew)
+import Utils (procToChan)
+import Control.Concurrent (readChan)
+
+delayMillis = 1000
+
+tpBattStatCmd = [ "/usr/lib/tpbattstat-applet/tpbattstat.py"
+                , "--json"
+                , show $ floor $ delayMillis]
+
+tpBattStatW = do
+  chan <- procToChan tpBattStatCmd
+  widget <- jsonWidgetNew $ readChan chan
+  return widget
