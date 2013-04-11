9cdec94e3ec6b6edcd98980b28f487d77a39c3fc
Elliot Wolk <elliot.wolk@gmail.com>
2013-04-11 16:44:16 -0400

    make a log widget using the pager widgets + workspace images
    has the workspace switcher on top of the window title, with layout on the end

---
+++ .config/taffybar/lib/WMLog.hs
@@ -0,0 +1,48 @@
+module WMLog (wmLogNew) where
+import WorkspaceSwitcherImages (workspaceSwitcherImagesNew)
+import Utils (fg, fgbg)
+
+import Graphics.UI.Gtk (
+  escapeMarkup, toWidget, hBoxNew, vBoxNew, containerAdd, widgetShowAll)
+
+import System.Taffybar.Pager (
+  PagerConfig(..), defaultPagerConfig,
+  colorize, shorten, wrap, escape, pagerNew)
+import System.Taffybar.LayoutSwitcher (layoutSwitcherNew)
+import System.Taffybar.WindowSwitcher (windowSwitcherNew)
+
+titleLength = 40
+
+bold m = "<b>" ++ m ++ "</b>"
+padTrim n x = take n $ x ++ repeat ' '
+
+pagerConfig = defaultPagerConfig
+  { activeWindow     = fg "green" . escapeMarkup . padTrim titleLength
+  , activeLayout     = \x -> case x of
+                               "left"    ->                   "[]="
+                               "top"     -> fgbg "blue" "red" "TTT"
+                               "full"    -> fgbg "blue" "red" "[ ]"
+                               otherwise -> fgbg "blue" "red" "???"
+  , activeWorkspace  = bold . fg "yellow" . escapeMarkup
+  , visibleWorkspace = escapeMarkup
+  , urgentWorkspace  = colorize "red" "yellow" . escapeMarkup
+  , widgetSep        = ""
+  }
+
+wmLogNew = do
+  pager <- pagerNew pagerConfig
+  ws <- workspaceSwitcherImagesNew pager
+  title <- windowSwitcherNew pager
+  layout <- layoutSwitcherNew pager
+
+  wsTitleBox <- vBoxNew True 0
+  containerAdd wsTitleBox ws
+  containerAdd wsTitleBox title
+
+  wmLogBox <- hBoxNew False 5
+  containerAdd wmLogBox wsTitleBox
+  containerAdd wmLogBox layout
+
+  widgetShowAll wmLogBox
+
+  return $ toWidget wmLogBox
