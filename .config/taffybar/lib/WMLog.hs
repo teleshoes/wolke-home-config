d8aabcf2db94c6c6612836b8a8666a6b1b96ad74
Elliot Wolk <elliot.wolk@gmail.com>
2013-04-12 02:19:56 -0400

    set border color in WMLog instead of workspaceSwitcher

--- .config/taffybar/lib/WMLog.hs
+++ .config/taffybar/lib/WMLog.hs
@@ -15,6 +15,8 @@ import System.Taffybar.Pager (
 import System.Taffybar.LayoutSwitcher (layoutSwitcherNew)
 import System.Taffybar.WindowSwitcher (windowSwitcherNew)
 
+wsBorderColor = Color 65535 0 0
+
 titleLength = 60
 
 bold m = "<b>" ++ m ++ "</b>"
@@ -36,12 +38,20 @@ pagerConfig pixbufs = defaultPagerConfig
   , widgetSep        = ""
   }
 
+applyToGrandChildren container cb = do
+  children <- containerGetChildren $ castToContainer container
+  mapM (\child -> containerForeach child cb) $ map castToContainer children
+
+setWsBorderColor w = widgetModifyBg w StateNormal wsBorderColor
+
 wmLogNew = do
   pixbufs <- loadImages
   pager <- pagerNew $ pagerConfig pixbufs
   ws <- wspaceSwitcherNew pager
   title <- windowSwitcherNew pager
   layout <- layoutSwitcherNew pager
+
+  applyToGrandChildren ws setWsBorderColor
 
   wsTitleBox <- vBoxNew True 0
   containerAdd wsTitleBox ws
