2d31580c564b625da03407eee5923957f8070531
Elliot Wolk <elliot.wolk@gmail.com>
2013-04-15 16:46:49 -0400

    set a fixed height only when resizing images
    rename icon dirs WxH => H

--- .config/taffybar/icons/convert.pl
+++ .config/taffybar/icons/convert.pl
@@ -2,52 +2,51 @@
 use strict;
 use warnings;
 
-my @sizes = ([18,18], [20,20], [24,24], [36,36], [40,40], [48,48], [64,64]);
+my @heights = (18, 20, 24, 36, 40, 48, 64);
 
 sub run(@){
   print "@_\n";
   system @_;
 }
 
-sub convertRsvg($$$$){
-  my ($src, $dest, $w, $h) = @_;
+sub convertRsvg($$$){
+  my ($src, $dest, $h) = @_;
   run "rsvg",
-    "-w", $w, "-h", $h,
+    "-h", $h,
+    "-a",
     "-f", "png",
     "-o", $dest,
     $src, $dest;
 }
-sub convertImageMagick($$$$){
-  my ($src, $dest, $w, $h) = @_;
-  run "convert",
-    "-resize", "${w}x${h}",
-    "-gravity", "center",
-    "-extent", "${w}x${h}",
-    $src, $dest;
+sub convertImageMagick($$$){
+  my ($src, $dest, $h) = @_;
+  run "convert", "-resize", "${h}", $src, $dest;
 }
 
 sub main(@){
-  my @imgs = `ls source/`;
+  my @imgs = `cd scalable; find -name '*.svg' -or -name '*.png'`;
 
-  my @sizeDirs = `find -mindepth 1 -maxdepth 1 -regex '.*/[0-9]+x[0-9]+'`;
+  my @sizeDirs = `find -mindepth 1 -maxdepth 1 -regex '.*/[0-9]+'`;
   for my $sizeDir(@sizeDirs){
     chomp $sizeDir;
     run "rm -r $sizeDir/";
   }
 
-  for my $size(@sizes){
-    my ($w, $h) = @$size;
-    my $dir = "./${w}x${h}";
-    run "mkdir", $dir;
+  for my $h(@heights){
+    my $dir = "./$h";
     for my $img(@imgs){
       chomp $img;
       my $dest = $img;
       $dest =~ s/\.[a-zA-Z0-9]+$/.png/i;
 
+      my $destDir = "$dir/$dest";
+      $destDir =~ s/\/[^\/]*$//;
+      run "mkdir", "-p", $destDir;
+
       if($img =~ /\.svg$/i){
-        convertRsvg "source/$img", "$dir/$dest", $w, $h;
+        convertRsvg "scalable/$img", "$dir/$dest", $h;
       }else{
-        convertImageMagick "source/$img", "$dir/$dest", $w, $h;
+        convertImageMagick "scalable/$img", "$dir/$dest", $h;
       }
     }
   }
