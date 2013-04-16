51ee10f2982c9be0243bea665d3f8f43956f3695
Elliot Wolk <elliot.wolk@gmail.com>
2013-04-16 00:38:51 -0400

    add a widgets module which exports all of my taffybar widgets

--- .config/taffybar/taffybar.hs
+++ .config/taffybar/taffybar.hs
@@ -1,27 +1,5 @@
-import Brightness(brightnessW)
-import Clock(clockW)
-import CpuFreqs(cpuFreqsW)
-import CpuScaling(cpuScalingW)
-import Ekiga(ekigaW)
-import Fan(fanW)
-import Fcrondyn(fcrondynW)
-import Klomp(klompW)
-import Mic(micW)
-import MonitorCpu(monitorCpuW)
-import MonitorMem(monitorMemW)
-import Net(netW)
-import NetStats(netStatsW)
-import Openvpn(openvpnW)
-import PidginPipe(pidginPipeW)
-import PingMonitor(pingMonitorW)
-import Sep(sepW)
-import TPBattStat(tpBattStatW)
-import Thunderbird(thunderbirdW)
-import Volume(volumeW)
-import WMLog(wmLogNew)
-
+import Widgets
 import Graphics.UI.Gtk.General.RcStyle (rcParseString)
-
 import System.Taffybar (defaultTaffybar, defaultTaffybarConfig,
   barHeight, widgetSpacing, startWidgets, endWidgets)
 
