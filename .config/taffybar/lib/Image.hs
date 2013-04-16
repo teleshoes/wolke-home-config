51bc31029957872c6ee4bbeb22120faa3a1fd475
Elliot Wolk <elliot.wolk@gmail.com>
2013-04-16 13:30:46 -0400

    dont update the pixbuf at all if the filepath hasnt changed

--- .config/taffybar/lib/Image.hs
+++ .config/taffybar/lib/Image.hs
@@ -5,18 +5,23 @@ import Utils (defaultDelay)
 import Graphics.UI.Gtk (
   imageNew, imageClear, imageSetFromPixbuf,
   realize, on, toWidget, postGUIAsync)
-import Control.Monad (forever, void)
+import Control.Monad (forever, unless, void)
 import Control.Concurrent (forkIO, threadDelay)
+import Control.Concurrent.MVar (newMVar, takeMVar, putMVar)
 import Control.Exception as E (catch, IOException)
 
 imageW cmd = do
   img <- imageNew
   pbCache <- pixbufCacheNew
+  lastFileMVar <- newMVar ""
   on img realize $ void $ forkIO $ forever $ do
     filepath <- cmd
-    pixbuf <- loadPixbuf pbCache filepath
-    postGUIAsync $ case pixbuf of
-      Just pb -> imageSetFromPixbuf img pb
-      Nothing -> imageClear img
+    prevFilepath <- takeMVar lastFileMVar
+    unless (filepath == prevFilepath) $ do
+      pixbuf <- loadPixbuf pbCache filepath
+      postGUIAsync $ case pixbuf of
+        Just pb -> imageSetFromPixbuf img pb
+        Nothing -> imageClear img
+    putMVar lastFileMVar filepath
     threadDelay $ floor (defaultDelay * 1000000)
   return img
