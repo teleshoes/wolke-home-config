81eeeb2803f66fa3b7d5fa1a90738469a6c783c8
Elliot Wolk <elliot.wolk@gmail.com>
2013-04-16 00:34:52 -0400

    pull taffybar.rc into taffybar.hs
    sacrifice changing without recompiling to having everything in one spot

--- .config/taffybar/taffybar.hs
+++ .config/taffybar/taffybar.hs
@@ -20,16 +20,22 @@ import Thunderbird(thunderbirdW)
 import Volume(volumeW)
 import WMLog(wmLogNew)
 
+import Graphics.UI.Gtk.General.RcStyle (rcParseString)
+
 import System.Taffybar (defaultTaffybar, defaultTaffybarConfig,
   barHeight, widgetSpacing, startWidgets, endWidgets)
 
 main = do
   let cfg = defaultTaffybarConfig {barHeight=36, widgetSpacing=5}
+      font = "Inconsolata medium 13"
+      bgColor = "#3f3c6d"
+      fgColor = "#a8aef7"
+      textColor = "#FFFFFF"
+
       titleLength = 30
       wsImageHeight = 24
       titleRows = True
       stackWsTitle = False
-
 
       start = [ wmLogNew titleLength wsImageHeight titleRows stackWsTitle ]
       end = reverse
@@ -59,4 +65,11 @@ main = do
           , clockW
           ]
 
+  rcParseString $ ""
+        ++ "style \"default\" {"
+        ++ "  font_name = \"" ++ font ++ "\""
+        ++ "  bg[NORMAL] = \"" ++ bgColor ++ "\""
+        ++ "  fg[NORMAL] = \"" ++ fgColor ++ "\""
+        ++ "  text[NORMAL] = \"" ++ textColor ++ "\""
+        ++ "}"
   defaultTaffybar cfg {startWidgets=start, endWidgets=end}
