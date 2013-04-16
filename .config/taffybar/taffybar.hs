a9f368fd2b7fdea0a90c48a7c8f96b80ae487e63
Elliot Wolk <elliot.wolk@gmail.com>
2013-04-16 15:52:53 -0400

    taffybar height 40=>38

--- .config/taffybar/taffybar.hs
+++ .config/taffybar/taffybar.hs
@@ -4,7 +4,7 @@ import System.Taffybar (defaultTaffybar, defaultTaffybarConfig,
   barHeight, widgetSpacing, startWidgets, endWidgets)
 
 main = do
-  let cfg = defaultTaffybarConfig {barHeight=40, widgetSpacing=5}
+  let cfg = defaultTaffybarConfig {barHeight=38, widgetSpacing=5}
       font = "Inconsolata medium 13"
       bgColor = "#3f3c6d"
       fgColor = "#a8aef7"
