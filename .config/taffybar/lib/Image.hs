3351a2b141b5d84dfe3773a243198dc3053f853e
Elliot Wolk <elliot.wolk@gmail.com>
2013-04-16 13:30:02 -0400

    implement a pixbufCache and use it for polling imageW

--- .config/taffybar/lib/Image.hs
+++ .config/taffybar/lib/Image.hs
@@ -1,28 +1,22 @@
 module Image(imageW) where
+import PixbufCache (pixbufCacheNew, loadPixbuf)
 import Utils (defaultDelay)
 
 import Graphics.UI.Gtk (
-  imageNew, imageNewFromFile, imageSetFromFile,
+  imageNew, imageClear, imageSetFromPixbuf,
   realize, on, toWidget, postGUIAsync)
-import Control.Monad (forever)
+import Control.Monad (forever, void)
 import Control.Concurrent (forkIO, threadDelay)
 import Control.Exception as E (catch, IOException)
 
-image file = do
-  img <- imageNewFromFile file
-  return $ toWidget img
-
 imageW cmd = do
   img <- imageNew
-  on img realize $ do
-    forkIO $ forever $ do
-      let tryUpdate = do
-            file <- cmd
-            postGUIAsync $ imageSetFromFile img file
-      E.catch tryUpdate ignoreIOException
-      threadDelay $ floor (defaultDelay * 1000000)
-    return ()
+  pbCache <- pixbufCacheNew
+  on img realize $ void $ forkIO $ forever $ do
+    filepath <- cmd
+    pixbuf <- loadPixbuf pbCache filepath
+    postGUIAsync $ case pixbuf of
+      Just pb -> imageSetFromPixbuf img pb
+      Nothing -> imageClear img
+    threadDelay $ floor (defaultDelay * 1000000)
   return img
-
-ignoreIOException :: IOException -> IO ()
-ignoreIOException _ = return ()
