a8112a7adb062c3f5301747140ab445ca1eeaf02
Elliot Wolk <elliot.wolk@gmail.com>
2013-07-15 02:38:51 -0400

    xmonad: add a simple xmonad-restart script to let me know when its done recompiling

--- .xmonad/lib/Bindings.hs
+++ .xmonad/lib/Bindings.hs
@@ -48,7 +48,7 @@ keyBinds conf = "Key Bindings" @@ mapM_ ($ conf)
 
 xmoKeys conf = "XMonad" @@ do
     "Restart Taffybar"  @@ mCA   xK_Home #! "taffybar-restart"
-    "Recompile Xmonad"  @@ mCA   xK_End  #! "xmonad --recompile; xmonad --restart"
+    "Recompile Xmonad"  @@ mCA   xK_End  #! "xmonad-restart"
     "Edit Keys"         @@ mCA   xK_Del  #! "term vim ~/.xmonad/lib/Bindings.hs"
 
 
