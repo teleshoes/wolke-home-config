f784dc5f9eb02fcfb803d4c94ba1ede625f529c7
Elliot Wolk <elliot.wolk@gmail.com>
2013-09-24 00:01:17 -0400

    taffybar[wmlog]: change active layout indent

--- .config/taffybar/lib/WMLog.hs
+++ .config/taffybar/lib/WMLog.hs
@@ -23,15 +23,15 @@ import System.Information.EWMHDesktopInfo (
 pagerConfig pixbufs cfg = defaultPagerConfig
   { activeWindow     = fg "#93a1a1" . escapeMarkup . fmtTitle cfg
   , activeLayout     = \x -> case x of
-                               "left"    -> return "[]="
-                               "top"     -> return $ fgbg "blue" "red" "TTT"
-                               "full"    -> do
-                                 cnt <- windowCount
-                                 let fmt = if 0 <= cnt && cnt < 10
-                                           then show cnt
-                                           else "+"
-                                 return $ fgbg "blue" "red" $ "[" ++ fmt ++ "]"
-                               otherwise -> return $ fgbg "blue" "red" "???"
+      "left"    -> return "[]="
+      "top"     -> return $ fgbg "blue" "red" "TTT"
+      "full"    -> do
+        cnt <- windowCount
+        let fmt = if 0 <= cnt && cnt < 10
+                  then show cnt
+                  else "+"
+        return $ fgbg "blue" "red" $ "[" ++ fmt ++ "]"
+      otherwise -> return $ fgbg "blue" "red" "???"
   , activeWorkspace  = wsStyle cfg (Just Red) $ bold . fgbg "#002b36" "#eee8d8"
   , hiddenWorkspace  = wsStyle cfg Nothing $ bold . fg "orange"
   , emptyWorkspace   = wsStyle cfg Nothing $ id
