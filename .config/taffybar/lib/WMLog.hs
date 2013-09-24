d591035533ef407f16f685cec70238a79f87850d
Elliot Wolk <elliot.wolk@gmail.com>
2013-09-24 00:02:44 -0400

    taffybar[wmlog]: only highlight full "[#]" if window count is > 1

--- .config/taffybar/lib/WMLog.hs
+++ .config/taffybar/lib/WMLog.hs
@@ -27,10 +27,9 @@ pagerConfig pixbufs cfg = defaultPagerConfig
       "top"     -> return "TTT"
       "full"    -> do
         cnt <- windowCount
-        let fmt = if 0 <= cnt && cnt < 10
-                  then show cnt
-                  else "+"
-        return $ fgbg "blue" "red" $ "[" ++ fmt ++ "]"
+        let numFmt = if 0 <= cnt && cnt < 10 then show cnt else "+"
+        let color = if cnt > 1 then fgbg "blue" "red" else id
+        return $ color $ "[" ++ numFmt ++ "]"
       otherwise -> return $ fgbg "blue" "red" "???"
   , activeWorkspace  = wsStyle cfg (Just Red) $ bold . fgbg "#002b36" "#eee8d8"
   , hiddenWorkspace  = wsStyle cfg Nothing $ bold . fg "orange"
