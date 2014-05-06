11c91a4ac8072c254f3a1aa5c4163da22090feb1
Elliot Wolk <elliot.wolk@gmail.com>
2014-05-06 05:20:34 -0400

    bootstrap: add dirs for syncing important git-ignored files

--- bootstrap/set-up-home-git.pl
+++ bootstrap/set-up-home-git.pl
@@ -3,9 +3,14 @@ use strict;
 use warnings;
 
 my $url = 'https://github.com/teleshoes/wolke-home-config.git';
+my @dirs = (
+);
 
 sub main(@){
-  system "nuc", "-u", "root", "apt-get install git";
+  my $host = `nuc`;
+  chomp $host;
+
+  system "nuc", "-u", "root", "apt-get install git rsync";
   system "nuc", "
     if [ ! -d .git ]; then
       git init
@@ -15,6 +20,9 @@ sub main(@){
       git checkout -t origin/nuc
     fi
   ";
+  for my $dir(@dirs){
+    system "rsync", "-avP", "$ENV{HOME}/$dir/", "$host:~/$dir/";
+  }
 }
 
 &main(@ARGV);
