3cdc4220a03cee16e8d22e2a5684d7e872e5f4e1
Elliot Wolk <elliot.wolk@gmail.com>
2014-05-14 14:49:03 -0400

    taffybar: make everything larger

--- .config/taffybar/taffybar.hs
+++ .config/taffybar/taffybar.hs
@@ -8,16 +8,16 @@ import System.Taffybar (defaultTaffybar, defaultTaffybarConfig,
   barHeight, widgetSpacing, startWidgets, endWidgets)
 
 main = do
-  let cfg = defaultTaffybarConfig {barHeight=38, widgetSpacing=5}
-      font = "Inconsolata medium 13"
+  let cfg = defaultTaffybarConfig {barHeight=42, widgetSpacing=5}
+      font = "Inconsolata medium 16"
       fgColor = hexColor $ RGB (0x93/0xff, 0xa1/0xff, 0xa1/0xff)
       bgColor = hexColor $ RGB (0x00/0xff, 0x2b/0xff, 0x36/0xff)
       textColor = hexColor $ Black
-      sep = W.sepW Black 2
+      sep = W.sepW Black 3
 
       start = [ W.wmLogNew WMLogConfig
                 { titleLength = 30
-                , wsImageHeight = 24
+                , wsImageHeight = 28
                 , titleRows = True
                 , stackWsTitle = False
                 , wsBorderColor = RGB (0.6, 0.5, 0.2)
