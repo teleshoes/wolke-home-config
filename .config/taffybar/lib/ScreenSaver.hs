39e2fe3553c24c000226fbef24bf28479a473fc4
Elliot Wolk <elliot.wolk@gmail.com>
2014-05-14 19:23:44 -0400

    taffybar[screensaver]: add an override file

--- .config/taffybar/lib/ScreenSaver.hs
+++ .config/taffybar/lib/ScreenSaver.hs
@@ -1,10 +1,12 @@
 module ScreenSaver(screenSaverW) where
 import Label (labelW)
-import Utils(readProc, padL)
+import Utils(chompFile, padL, readProc)
 
 import Control.Concurrent (forkIO, threadDelay, readChan, writeChan, newChan)
 import Control.Monad (void)
 import System.Process (system)
+
+overrideFile = "/tmp/screen-saver-override"
 
 checkDelayMillis = 1 * 1000
 idleTimeoutMillis = 10 * 60 * 1000
@@ -17,15 +19,22 @@ screenSaverW = do
 
 checkScreenSaver chan prevState = do
   xidle <- getXidle
-  let state = xidle > idleTimeoutMillis
+  override <- getOverride
+  let state = case override of
+                "off" -> False
+                _     -> xidle > idleTimeoutMillis
   let timeoutS = (idleTimeoutMillis - xidle) `div` 1000
-  let msg = padL ' ' 4 $ if state then "SCRN" else show timeoutS
+  let msg = padL ' ' 4 $ case override of
+                            "off" -> "off"
+                            _     -> if state then "SCRN" else show timeoutS
   writeChan chan $ msg ++ "\nidle"
   if state && not prevState then screenSaverOn else return ()
   if not state && prevState then screenSaverOff else return ()
 
   threadDelay $ checkDelayMillis * 10^3
   checkScreenSaver chan $ state
+
+getOverride = chompFile overrideFile
 
 screenSaverOn = void $ system "brightness 50"
 screenSaverOff = void $ system "brightness 100"
