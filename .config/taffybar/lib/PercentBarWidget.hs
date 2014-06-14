b0a0f76651e9f07c6201d7c5050410f8f6d7b704
Elliot Wolk <elliot.wolk@gmail.com>
2014-06-14 01:00:25 -0400

    taffybar[percentbar]: pull color selection into percent bar, use finite color list
    fixes out of memory exception for large bar values

--- .config/taffybar/lib/PercentBarWidget.hs
+++ .config/taffybar/lib/PercentBarWidget.hs
@@ -1,5 +1,5 @@
 module PercentBarWidget (
-  percentBarWidgetW, percentBarConfig, cycleColors
+  percentBarWidgetW, percentBarConfig
 ) where
 import Graphics.UI.Gtk (realize, on)
 import System.Taffybar.Widgets.VerticalBar (
@@ -7,15 +7,7 @@ import System.Taffybar.Widgets.VerticalBar (
   verticalBarNew, verticalBarSetColors, verticalBarSetPercent,
   BarConfig(..), defaultBarConfig)
 import Control.Concurrent (threadDelay, forkIO)
-import Control.Monad (forever, void)
-
-constColor c _ = c
-
-cycleColors (bg:fg:[]) _ = (bg, fg)
-cycleColors (bg:fg:cs) p | p > 1 = cycleColors (fg:cs) (p-1)
-                         | otherwise = (bg, fg)
-
-percent p = if p > 1 then percent (p-1) else p
+import Control.Monad (forever, when, void)
 
 percentBarConfig = cfg { barWidth = 10 }
   where cfg = defaultBarConfig (constColor (0,0,0))
@@ -24,8 +16,22 @@ percentBarWidgetW cfg interval barReader = do
   (w, vbh) <- verticalBarNew cfg
   on w realize $ do
     void $ forkIO $ forever $ do
-      (fg, bg, p) <- barReader
+      (p, colors) <- barReader
+      checkLen "too many colors" colors 100
+      let (bg, fg) = selectColors colors p
       verticalBarSetColors vbh (constColor fg) (constColor bg)
-      verticalBarSetPercent vbh (percent p)
+      verticalBarSetPercent vbh (zeroToOne p)
       threadDelay $ floor $ interval * 10^6
   return w
+
+zeroToOne p | f == 0 = if n > 0 then 1.0 else 0.0
+            | otherwise = f
+  where (n, f) = properFraction p
+
+checkLen msg xs len = when (length (take (len+1) xs) > len) (error msg)
+
+constColor c _ = c
+
+selectColors colors p = (cs !! i, cs !! (i+1))
+  where cs = cycle colors
+        i = (max 0 ((ceiling p) - 1)) `mod` (length colors)
