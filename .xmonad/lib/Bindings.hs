4fae27451cbbac68415a40dec3f03f109e240ffe
Elliot Wolk <elliot.wolk@gmail.com>
2014-08-02 15:51:14 -0400

    xmonad; write keybindings at startup

--- .xmonad/lib/Bindings.hs
+++ .xmonad/lib/Bindings.hs
@@ -14,11 +14,16 @@ import qualified Data.Foldable as F
 import Data.Map ((!))
 import qualified Data.Map as M
 import Data.Maybe
+import System.IO.Error (catchIOError)
 
 import Bindings.Keys
 import Bindings.Writer
 
 main = putStr . prettyBindings $ keyBinds testConfig
+
+tryWriteKeyBindingsCache file = writeKeyBindingsCache file `catchIOError` print
+writeKeyBindingsCache file = writeFile file fmt
+  where fmt = prettyBindingsCL $ keyBinds testConfig
 
 myMouseBindings = M.fromList . bwBindList . mouseBinds
 myKeyBindings   = M.fromList . bwBindList . keyBinds
