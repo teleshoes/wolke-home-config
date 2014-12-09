e7fc316619cbd0ee18e1438b76c96c345c1aa0d3
Elliot Wolk <elliot.wolk@gmail.com>
2014-12-09 11:11:50 -0500

    xmonad: bindings klomp-display => klomp-pi-display

--- .xmonad/lib/Bindings.hs
+++ .xmonad/lib/Bindings.hs
@@ -192,8 +192,8 @@ shortcuts conf = "Shortcuts" @@ do
         "seek 10"     @@ mCAWS xK_x    #! "nuc klomp-cmd seek 10"
         "seek -60"    @@ mCAWS xK_a    #! "nuc klomp-cmd seek -60"
         "seek 60"     @@ mCAWS xK_s    #! "nuc klomp-cmd seek 60"
-        "display off" @@ mCAW  xK_c    #! "nuc klomp-display --off; klomp-bar local"
-        "display on"  @@ mCAW  xK_v    #! "nuc klomp-display --on; klomp-bar nuc"
+        "display off" @@ mCAW  xK_c    #! "nuc klomp-pi-display --off; klomp-bar local"
+        "display on"  @@ mCAW  xK_v    #! "nuc klomp-pi-display --on; klomp-bar nuc"
 
     "Klomp N9"      @@ do
         "pause"       @@ mCW   (xK ' ')#! "n9 -b udo klomp-cmd pause"
