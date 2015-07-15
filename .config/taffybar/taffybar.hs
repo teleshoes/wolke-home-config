cb5cb77de4b066dbfff006dafaa6318a36114ddb
Elliot Wolk <elliot.wolk@gmail.com>
2015-07-15 16:11:37 -0400

    taffybar: change ping monitor dev.escribe.com => garuda.escribe.com

--- .config/taffybar/taffybar.hs
+++ .config/taffybar/taffybar.hs
@@ -52,7 +52,7 @@ main = do
           , W.fanW
           , W.brightnessW
           , colW [ W.pingMonitorW "G" "www.google.com"
-                 , W.pingMonitorW "E" "dev.escribe.com"
+                 , W.pingMonitorW "E" "garuda.escribe.com"
                  , W.pingMonitorW "A" "ehr.dev"
                  ]
           , colW [ W.openvpnW "escribe"
