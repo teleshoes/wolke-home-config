53de7ce45874987c80f4daba2d7c07e6e6e7ee55
Elliot Wolk <elliot.wolk@gmail.com>
2016-06-08 16:51:11 -0400

    greasemonkey[megabus]: add megabus autologin+auto-form-entry bos<=>nyc

--- greasemonkey/gen-config.pl
+++ greasemonkey/gen-config.pl
@@ -14,6 +14,7 @@ my $scripts = [
   ["multiverse",          "http://amultiverse.com/*",                                 "hide scenes from a multiverse header"],
   ["boltbus",             "https://www.boltbus.com/*?*",                              "sets up boston=&gt;ny"],
   ["gobus",               "https://gobuses.com/*?gm-*",                               "boston&lt;=&gt;nyc"],
+  ["megabus",             "https://us.megabus.com/*?gm-*",                            "boston&lt;=&gt;nyc"],
   ["chase",               "https://chaseonline.chase.com/Logon*",                     "chase logon"],
   ["github-username",     "https://github.com/*",                                     ""],
 ];
