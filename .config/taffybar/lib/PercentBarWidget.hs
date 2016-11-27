596ad2a4d273ce4cca101e921f3f0b883ef04ef1
Elliot Wolk <elliot.wolk@gmail.com>
2016-11-26 21:54:21 -0500

    taffybar[percentbar]: rewrite percentbar colors with BarConfigIO instead of verticalBarSetColors

--- .config/taffybar/lib/PercentBarWidget.hs
+++ .config/taffybar/lib/PercentBarWidget.hs
@@ -4,10 +4,14 @@ module PercentBarWidget (
 import Graphics.UI.Gtk (realize, on)
 import System.Taffybar.Widgets.VerticalBar (
   VerticalBarHandle,
-  verticalBarNew, verticalBarSetColors, verticalBarSetPercent,
-  BarConfig(..), defaultBarConfig)
-import Control.Concurrent (threadDelay, forkIO)
+  verticalBarNew, verticalBarSetPercent,
+  BarConfig(..), defaultBarConfigIO)
+import Control.Concurrent (MVar, newEmptyMVar, tryPutMVar, tryTakeMVar, threadDelay, forkIO)
 import Control.Monad (forever, when, void)
+import Data.Maybe (fromMaybe)
+
+type VBColor = (Double, Double, Double)
+type VBColorReader = Double -> IO VBColor
 
 mainPercentBarWidget interval barReader = forever $ do
   (p, colors) <- barReader
@@ -16,20 +20,36 @@ mainPercentBarWidget interval barReader = forever $ do
   print (p, bg, fg)
   threadDelay $ floor $ interval * 10^6
 
-percentBarConfig = cfg { barWidth = 10 }
-  where cfg = defaultBarConfig (constColor (0,0,0))
+percentBarConfig :: VBColorReader -> VBColorReader -> BarConfig
+percentBarConfig bgReader fgReader = cfg { barWidth = 10
+                                         , barColorIO = fgReader
+                                         , barBackgroundColorIO = bgReader
+                                         }
+  where cfg = defaultBarConfigIO (const $ return (0, 0, 0))
 
-percentBarWidgetW cfg interval barReader = do
-  (w, vbh) <- verticalBarNew cfg
+percentBarWidgetW interval barReader = do
+  bgColorMVar <- newEmptyMVar
+  fgColorMVar <- newEmptyMVar
+  let bgColorReader = colorReader bgColorMVar (0, 0, 0)
+  let fgColorReader = colorReader fgColorMVar (0, 0, 0)
+  (w, vbh) <- verticalBarNew $ percentBarConfig bgColorReader fgColorReader
+
   on w realize $ do
     void $ forkIO $ forever $ do
       (p, colors) <- barReader
       checkLen "too many colors" colors 100
       let (bg, fg) = selectColors colors p
-      verticalBarSetColors vbh (constColor fg) (constColor bg)
+      tryPutMVar bgColorMVar bg
+      tryPutMVar fgColorMVar fg
       verticalBarSetPercent vbh (zeroToOne p)
       threadDelay $ floor $ interval * 10^6
   return w
+
+colorReader :: MVar VBColor -> VBColor -> VBColorReader
+colorReader mvar defaultColor = const $ do
+  maybeColor <- tryTakeMVar mvar
+  let color = fromMaybe defaultColor maybeColor
+  return color
 
 zeroToOne p | f == 0 = if n > 0 then 1.0 else 0.0
             | otherwise = f
@@ -37,8 +57,7 @@ zeroToOne p | f == 0 = if n > 0 then 1.0 else 0.0
 
 checkLen msg xs len = when (length (take (len+1) xs) > len) (error msg)
 
-constColor c _ = c
-
+selectColors :: [VBColor] -> Double -> (VBColor, VBColor)
 selectColors colors p = (cs !! i, cs !! (i+1))
   where cs = cycle colors
         i = (max 0 ((ceiling p) - 1)) `mod` (length colors)
