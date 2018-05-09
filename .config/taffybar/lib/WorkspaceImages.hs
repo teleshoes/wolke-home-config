7ab7e2c102981d1f882b73b23c399868a99122a1
Elliot Wolk <elliot.wolk@gmail.com>
2018-05-09 15:55:02 -0400

    taffybar: 2.0.0 migrate workspace images

--- .config/taffybar/lib/WorkspaceImages.hs
+++ .config/taffybar/lib/WorkspaceImages.hs
@@ -1,17 +1,55 @@
 {-# LANGUAGE FlexibleContexts #-}
-module WorkspaceImages (getIcons, selectImage, selectImageName) where
+module WorkspaceImages (getIcon, loadNamedIconFileMap) where
 import Utils (imageDir, tryMaybe)
 
+import System.Taffybar.Widget.Workspaces (
+  WorkspacesIO, IconInfo(..), WindowData(..),
+  defaultGetIconInfo, liftX11Def)
+import System.Taffybar.Information.EWMHDesktopInfo (
+  EWMHIconData, getWindowIconsData)
+
 import System.Directory (listDirectory)
+import Control.Monad.Trans (liftIO)
 import Data.Char (toLower)
-import Data.List (isInfixOf, isPrefixOf, isSuffixOf)
+import Data.Text (stripSuffix)
+import Data.List (dropWhile, isInfixOf, isPrefixOf, isSuffixOf)
+import Data.List.Utils (replace)
 import Data.Maybe (fromMaybe)
 import Data.Maybe (listToMaybe, catMaybes)
 
 import System.Environment (getEnv)
 
-getIcons :: Int -> IO [(String, FilePath)]
-getIcons h = do
+type NamedIconFileMap = [(String, FilePath)]
+type IconName = String
+
+getIcon :: NamedIconFileMap -> WindowData -> WorkspacesIO IconInfo
+getIcon namedIconFileMap windowData = do
+  icon <- applyUntilJust [getOverrideIcon, getEwmhIcon, getTitleClassIcon]
+  return $ fromMaybe IINone icon
+  where getOverrideIcon = return $ fmap IIFilePath $ nameToFile $ overrideIconName
+        getEwmhIcon = fmap (fmap IIEWMH) $ getEwmhIconData windowData
+        getTitleClassIcon = return $ fmap IIFilePath $ nameToFile $ titleClassIconName
+
+        overrideIconName = getOverrideIconName winTitle winClass
+        titleClassIconName = getTitleClassIconName namedIconFileMap winTitle winClass
+
+        nameToFile (Just iconName) = lookup iconName namedIconFileMap
+        nameToFile Nothing = Nothing
+        (winTitle, winClass) = (windowTitle windowData, windowClass windowData)
+
+applyUntilJust :: Monad m => [m (Maybe a)] -> m (Maybe a)
+applyUntilJust [] = return Nothing
+applyUntilJust (act:acts) = do
+  res <- act
+  case res of
+    Just val -> return $ Just val
+    Nothing  -> applyUntilJust acts
+
+getEwmhIconData :: WindowData -> WorkspacesIO (Maybe EWMHIconData)
+getEwmhIconData windowData = liftX11Def Nothing (getWindowIconsData $ windowId windowData)
+
+loadNamedIconFileMap :: Int -> IO NamedIconFileMap
+loadNamedIconFileMap h = do
   dir <- wsImageDir h
   files <- fmap (fromMaybe []) $ tryMaybe $ listDirectory dir
   let pngs = filter (".png" `isSuffixOf`) files
@@ -26,7 +64,8 @@ contains = flip isInfixOf
 startsWith = flip isPrefixOf
 endsWith = flip isSuffixOf
 
-getOverride winTitle winClass
+getOverrideIconName :: String -> String -> Maybe IconName
+getOverrideIconName winTitle winClass
   | null winTitle && null winClass              = Just "blank"
   | winTitle == "..." && winClass == "..."      = Just "blank"
   | winTitle `endsWith` " - VIM"                = Just "vim"
@@ -40,19 +79,15 @@ getOverride winTitle winClass
   | otherwise = Nothing
   where ffPage s pageName = s `startsWith` pageName && s `endsWith` "MozillaFirefox"
 
-selectImageName :: [String] -> Bool -> String -> String -> Maybe String
-selectImageName imgNames hasIcon winTitle winClass = listToMaybe $ catMaybes maybeNames
-  where nTitle = listToMaybe $ filter (lcTitle `contains`) imgNames
-        nClass = listToMaybe $ filter (lcClass `contains`) imgNames
-        nOverride = getOverride winTitle winClass
-        nUnknown = Just "unknown"
-        maybeNames = if hasIcon then [nOverride] else [nOverride, nClass, nTitle, nUnknown]
-        lcTitle = map toLower winTitle
-        lcClass = map toLower winClass
+getTitleClassIconName :: NamedIconFileMap -> String -> String -> Maybe IconName
+getTitleClassIconName namedIconFileMap winTitle winClass = listToMaybe iconNames
+  where titleIconName = searchNamedIconFileMap namedIconFileMap winTitle
+        classIconName = searchNamedIconFileMap namedIconFileMap winClass
+        unknownIconName = Just "unknown"
+        iconNames = catMaybes [titleIconName, classIconName, unknownIconName]
 
-selectImage :: [(String, FilePath)] -> Bool -> String -> String -> Maybe FilePath
-selectImage icons hasIcon winTitle winClass = maybeFilePath maybeName
-  where imageNames = map fst icons
-        maybeName = selectImageName imageNames hasIcon winTitle winClass
-        maybeFilePath (Just name) = lookup name icons
-        maybeFilePath Nothing = Nothing
+searchNamedIconFileMap :: NamedIconFileMap -> String -> Maybe IconName
+searchNamedIconFileMap namedIconFileMap query = listToMaybe matchingIconNames
+  where iconNames = map fst namedIconFileMap :: [IconName]
+        lcQuery = map toLower query
+        matchingIconNames = filter (lcQuery `contains`) iconNames
