8354c1d01e8053d154dfa9219b1b11c4fd6278d9
Elliot Wolk <elliot.wolk@gmail.com>
2018-06-11 14:36:13 -0400

    taffybar: dynamically calculate profiles using ~/resconfig-screen

--- .config/taffybar/taffybar.hs
+++ .config/taffybar/taffybar.hs
@@ -1,6 +1,7 @@
 import qualified Widgets as W
 import Color (Color(..), hexColor)
-import Utils (colW, attemptCreateSymlink)
+import Utils (attemptCreateSymlink, colW,
+  getHomeFile, maybeJoin, readInt, regexFirstGroup, tryMaybe)
 import Width (charsFitInPx, getScreenDPI, screenPctToPx)
 
 import Control.Monad.Trans (liftIO)
@@ -9,18 +10,13 @@ import System.Taffybar.SimpleConfig (simpleTaffybar, defaultSimpleTaffyConfig,
   Position(Top, Bottom))
 
 import Data.Functor ((<$>))
+import Data.Maybe (fromMaybe)
 import System.Environment (getArgs)
 import System.Environment.XDG.BaseDir ( getUserConfigFile )
 
-profile = profileWQHD
-
---                 pName  barHt  wImgH  space  wSepW  title  fontP  graph  music
-profileWQHD  = P  "wqhd"     50     28      5      3     30   17.0     80  15.94
-profileT430s = P "t430s"     38     24      5      2     30   13.0     50  15.94
-profileW520  = P  "w520"     38     24      5      2     30   13.0     50  15.63
-profileNuc   = P   "nuc"     48     28      5      3     30   16.0     50  19.43
-
 typeface = "Inconsolata"
+
+getResconfigFile = getHomeFile "resconfig-screen"
 
 data Profile = P { pName :: String --profile name
                  , barHt :: Int    --bar height in pixels
@@ -31,9 +27,44 @@ data Profile = P { pName :: String --profile name
                  , fontP :: Double --font point size
                  , graph :: Int    --width of graphs in pixels
                  , music :: Double --percent of the screen width to use for song info
-                 }
+                 } deriving Show
+
+data Resconfig = R { name       :: String --screen/monitor name
+                   , widthPx    :: Int    --horizontal resolution
+                   , heightPx   :: Int    --vertical resolution
+                   , widthMM    :: Int    --horizontal screen size
+                   , heightMM   :: Int    --vertical screen size
+                   , distanceMM :: Int    --expected viewing distance
+                   } deriving Show
+
+calculateProfile resconfig = P
+  { pName = name resconfig
+  , barHt = int $ scaleFHD_WQHD 38 50 + scaleLAP_WALL 0 10
+  , wImgH = int $ if isBig then 28 else 24
+  , space = int $ 5
+  , wSepW = int $ 2 + (scaleLAP_WALL 0 1) + (scaleFHD_WQHD 0 1)
+  , title = int $ 30
+  , fontP = dbl $ scaleFHD_WQHD 13.0 17.0 + scaleLAP_WALL 0 3.0
+  , graph = int $ scaleFHD_WQHD 50 80
+  , music = dbl $ scale14IN_60IN 15.94 19.43
+  }
+  where scaleFHD_WQHD = scale (widthPx resconfig, 1920, 2560)
+        scaleLAP_WALL = scale (distanceMM resconfig, 500, 1700)
+        scale14IN_60IN = scale (widthMM resconfig, 310, 2253)
+        isBig = widthPx resconfig > 1920 || distanceMM resconfig > 1000
+        int = round :: RealFrac n => n -> Int
+        dbl = id :: Double -> Double
+
+scale :: RealFrac n => (Int, Int, Int) -> n -> n -> n
+scale (factorVal, factorLow, factorHigh) low high = low + factor*(high-low)
+  where factor = (fVal - fLow) / (fHigh - fLow)
+        [fVal, fLow, fHigh] = map fromIntegral [factorVal, factorLow, factorHigh]
 
 main = do
+  resconfig <- readResconfigScreen
+  let profile = calculateProfile resconfig
+  print resconfig
+  print profile
   dpi <- getScreenDPI
   isBot <- elem "--bottom" <$> getArgs
   klompWidthPx <- screenPctToPx $ music profile
@@ -99,3 +130,21 @@ main = do
         ++ "}\n"
 
   simpleTaffybar cfg {startWidgets=start, endWidgets=end}
+
+readResconfigScreen :: IO Resconfig
+readResconfigScreen = fmap (fromMaybe defaultResconfig) $ tryMaybe $ do
+  resconfigFile <- getResconfigFile
+  str <- readFile resconfigFile
+  return $ R { name       = def name       $ getStr "name" str
+             , widthPx    = def widthPx    $ getInt "width_px" str
+             , heightPx   = def heightPx   $ getInt "height_px" str
+             , widthMM    = def widthMM    $ getInt "width_mm" str
+             , heightMM   = def heightMM   $ getInt "height_mm" str
+             , distanceMM = def distanceMM $ getInt "distance_mm" str
+             }
+  where defaultResconfig = R "default" 1920 1080 344 194 500
+        def field = fromMaybe $ field defaultResconfig
+        getStr :: String -> String -> Maybe String
+        getStr field str = regexFirstGroup (field ++ "\\s*=\\s*(.*)") str
+        getInt :: String -> String -> Maybe Int
+        getInt field str = fmap fromIntegral $ maybeJoin readInt $ getStr field str
