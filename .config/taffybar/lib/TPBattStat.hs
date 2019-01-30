d50a7590e5f8a2e3d5c0b2c8db12e77e94c8cca3
Elliot Wolk <elliot.wolk@gmail.com>
2019-01-30 15:22:37 -0500

    taffybar[tpbattstat]: use largest icon available smaller than barHeight (instead of just assuming barHeight is exactly an available size)

--- .config/taffybar/lib/TPBattStat.hs
+++ .config/taffybar/lib/TPBattStat.hs
@@ -2,12 +2,19 @@ module TPBattStat (tpBattStatW) where
 import JsonWidget (jsonWidgetNew)
 import Utils (defaultDelay, procToChan)
 import Control.Concurrent (readChan)
+import Data.Maybe (fromMaybe, listToMaybe)
+
+availableIconSizes = [18, 20, 24, 36, 38, 40, 48, 50, 64] :: [Int]
 
 tpBattStatCmd h = [ "/usr/lib/tpbattstat-applet/tpbattstat.py"
                   , "--json"
                   , show $ floor $ defaultDelay * 1000
-                  , show h ++ "x" ++ show h
+                  , sizeFmt
                   ]
+  where okSizes = filter (<=h) availableIconSizes
+        smallestSize = minimum availableIconSizes
+        size = maximum (smallestSize:okSizes)
+        sizeFmt = show size ++ "x" ++ show size
 
 tpBattStatW h = do
   chan <- procToChan $ tpBattStatCmd h
