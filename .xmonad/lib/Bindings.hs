957a73cd0923a0ce64cb1f993f1e319c2b25c1cc
Elliot Wolk <elliot.wolk@gmail.com>
2020-12-08 14:21:55 -0500

    xmonad[bindings]: add binding ctrl+alt+d => stepmania

--- .xmonad/lib/Bindings.hs
+++ .xmonad/lib/Bindings.hs
@@ -225,6 +225,7 @@ shortcuts conf = "Shortcuts" @@ do
         "Eclipse"       @@ mCA   xK_e    #! "eclipse"
         "FBreader"      @@ mCA   xK_r    #! "fbreader"
         "sheetmusic"    @@ mCA   xK_s    #! "sheetmusic"
+        "stepmania"     @@ mCA   xK_d    #! "stepmania"
         "stepmania -i"  @@ mCA   xK_i    #! "stepmania -i"
         "smbc"          @@ mCA   xK_m    #! "smbc"
 
