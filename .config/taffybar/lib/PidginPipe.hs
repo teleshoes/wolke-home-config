c30af6fe7d4a455a1de6c8135027ec7791e45462
Elliot Wolk <elliot.wolk@gmail.com>
2021-04-06 12:53:48 -0400

    taffybar[pidgin]: alternate flashing icon for 'new message' state

--- .config/taffybar/lib/PidginPipe.hs
+++ .config/taffybar/lib/PidginPipe.hs
@@ -1,14 +1,21 @@
 module PidginPipe(pidginPipeW) where
 import Clickable (clickableAsync)
 import Label (mainLabel)
-import Image (imageW)
+import Image (imageWithStateW)
 import System.Environment (getEnv)
 import Control.Monad (forever)
 import Data.Char (toLower)
+import Data.IORef (IORef, newIORef, readIORef, writeIORef)
 import Utils (getHomeFile, ifM, selectClosestImageDir, chompAll, isRunning, chompFile)
 
-main = mainLabel $ getImage 0
-pidginPipeW h = clickableAsync clickL clickM clickR =<< imageW (getImage h)
+main = do
+  flashToggle <- newToggle
+  mainLabel $ getImage 0 flashToggle
+
+pidginPipeW h = do
+  flashToggle <- newToggle
+  img <- imageWithStateW flashToggle (getImage h)
+  clickableAsync clickL clickM clickR img
 
 exec = "pidgin"
 process = exec
@@ -23,22 +30,34 @@ clickL = ifM (isRunning process) (return $ Just wsCmd) (return $ Just runCmd)
 clickM = return $ Just rerunCmd
 clickR = return $ Just killCmd
 
-getImage :: Int -> IO String
-getImage h = do
+getImage :: Int -> IORef Bool -> IO String
+getImage h flashToggle = do
   pipeFile <- getHomeFile ".purple/plugins/pipe"
 
   pipe <- chompFile pipeFile
   let status = if null pipe then "off" else map toLower pipe
 
+  isFlash <- nextToggle flashToggle
+
   pidginRunning <- if status == "off" then return False else isRunning exec
 
   dir <- selectClosestImageDir h
-  let img = if pidginRunning then imgName status else imgName "off"
+  let img = if pidginRunning then imgName status isFlash else imgName "off" isFlash
   return $ dir ++ "/pidgin/" ++ img ++ ".png"
 
-imgName status = case status of
+newToggle :: IO (IORef Bool)
+newToggle = newIORef False
+
+nextToggle :: IORef Bool -> IO Bool
+nextToggle toggle = do
+  curVal <- readIORef toggle
+  let nextVal = not curVal
+  writeIORef toggle nextVal
+  return nextVal
+
+imgName status isFlash = case status of
   "off"            -> "not-running"
-  "new message"    -> "pidgin-tray-pending"
+  "new message"    -> if isFlash then "pidgin-tray-pending-flash" else "pidgin-tray-pending"
   "available"      -> "pidgin-tray-available"
   "away"           -> "pidgin-tray-away"
   "do not disturb" -> "pidgin-tray-busy"
