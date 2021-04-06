5ae99ec91921270ccd6d6ee25dadce8907e86fd4
Elliot Wolk <elliot.wolk@gmail.com>
2021-04-06 12:34:07 -0400

    taffybar[image]: add optional state parameter to image commands

--- .config/taffybar/lib/Image.hs
+++ .config/taffybar/lib/Image.hs
@@ -1,7 +1,8 @@
-module Image(imageW) where
+module Image(imageW, imageWithStateW) where
 import PixbufCache (pixbufCacheNew, loadPixbuf)
 import Utils (defaultDelay)
 
+import Data.IORef (IORef, newIORef)
 import Data.Maybe (isJust)
 
 import Data.GI.Gtk.Threading (postGUIASync)
@@ -16,11 +17,16 @@ import Control.Exception as E (catch, IOException)
 
 imageW :: (IO String) -> IO Image
 imageW cmd = do
+  emptyState <- newIORef 0
+  imageWithStateW emptyState (\_ -> cmd)
+
+imageWithStateW :: IORef a -> (IORef a -> IO String) -> IO Image
+imageWithStateW state cmdWithState = do
   img <- imageNew
   pbCache <- pixbufCacheNew
   lastFileMVar <- newMVar ""
   onWidgetRealize img $ void $ forkIO $ forever $ do
-    filepath <- cmd
+    filepath <- cmdWithState state
     prevFilepath <- takeMVar lastFileMVar
     unless (filepath == prevFilepath) $ do
       pixbuf <- loadPixbuf pbCache filepath
