d3de602b59fac87d8831ddc2856e6f101993b257
Elliot Wolk <elliot.wolk@gmail.com>
2025-06-18 12:41:44 -0400

    vim: add usage and arg parsing to default perl template

--- .vim/templates/default.pl
+++ .vim/templates/default.pl
@@ -1,9 +1,25 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use File::Basename qw(basename);
+
+my $EXEC = basename $0;
+
+my $USAGE = "Usage:
+  $EXEC -h|--help
+    show this message
+";
 
 sub main(@){
-
+  while(@_ > 0){
+    my $arg = shift;
+    if($arg =~ /^(-h|--help)$/){
+      print $USAGE;
+      exit 0;
+    }else{
+      die "ERROR: unknown arg $arg\n";
+    }
+  }
 }
 
 &main(@ARGV);
